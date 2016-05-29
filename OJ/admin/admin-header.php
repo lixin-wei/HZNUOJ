207680825d29ce1021e30f91ba5691c09ef3b916
D_Star <wlx65005@163.com>
2016-05-29 13:07:29 +0800

    add script to admin page

--- OJ/admin/admin-header.php
+++ OJ/admin/admin-header.php
@@ -8,9 +8,18 @@
 
 <?php @session_start();?>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<!--
 <link rel=stylesheet href='../include/hoj.css' type='text/css'>
+-->
 <script src="../template/bs3/jquery.min.js"></script>
+<!-- 新 Bootstrap 核心 CSS 文件 -->
 <link rel="stylesheet" href="//cdn.bootcss.com/bootstrap/3.3.5/css/bootstrap.min.css">
+
+<!-- jQuery文件。务必在bootstrap.min.js 之前引入 -->
+<script src="//cdn.bootcss.com/jquery/1.11.3/jquery.min.js"></script>
+
+<!-- 最新的 Bootstrap 核心 JavaScript 文件 -->
+<script src="//cdn.bootcss.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 <script>
   $("document").ready(function (){
     $("form").append("<div id='csrf' />");
--- OJ/admin/index.php
+++ OJ/admin/index.php
@@ -25,44 +25,43 @@ if(isset($OJ_LANG)) require_once("../lang/$OJ_LANG.php");
     <div class="row">
       <div class="col-md-2 sidebar">
         <center>
-          <div class="well sidebar-nav">
             <ul class="nav nav-pills nav-stacked">
-              <li><a href="watch.php" target="main"><b><?php echo $MSG_SEEOJ?></b></a></li>
+              <li class="active"><a href="watch.php" target="main"><?php echo $MSG_SEEOJ?></a></li>
         <?php 
           if ($GE_TA) {
         ?>
-            <li><a href="problem_add_page.php?type=C" target="main"><b><?php echo "AddCProblem" ?></b></a></li>
-            <li><a href="problem_list.php?type=C" target="main"><b><?php echo "CProblemList" ?></b></a></li>
+            <li><a href="problem_add_page.php?type=C" target="main"><?php echo "AddCProblem" ?></a></li>
+            <li><a href="problem_list.php?type=C" target="main"><?php echo "CProblemList" ?></a></li>
         <?php 
           }
           if ($GE_T) {
         ?>
-            <li><a href="problem_add_page.php" target="main"><b><?php echo $MSG_ADD.$MSG_PROBLEM?></b></a></li>
-            <li><a href="problem_list.php" target="main"><b><?php echo $MSG_PROBLEM.$MSG_LIST?></b></a></li>
-            <li><a href="contest_add.php" target="main"><b><?php echo $MSG_ADD.$MSG_CONTEST?></b></a></li>
-            <li><a href="contest_list.php" target="main"><b><?php echo $MSG_CONTEST.$MSG_LIST?></b></a></li>
-            <li><a href="news_add_page.php" target="main"><b><?php echo $MSG_ADD.$MSG_NEWS?></b></a></li>
-            <li><a href="news_list.php" target="main"><b><?php echo $MSG_NEWS.$MSG_LIST?></b></a>
+            <li><a href="problem_add_page.php" target="main"><?php echo $MSG_ADD.$MSG_PROBLEM?></a></li>
+            <li><a href="problem_list.php" target="main"><?php echo $MSG_PROBLEM.$MSG_LIST?></a></li>
+            <li><a href="contest_add.php" target="main"><?php echo $MSG_ADD.$MSG_CONTEST?></a></li>
+            <li><a href="contest_list.php" target="main"><?php echo $MSG_CONTEST.$MSG_LIST?></a></li>
+            <li><a href="news_add_page.php" target="main"><?php echo $MSG_ADD.$MSG_NEWS?></a></li>
+            <li><a href="news_list.php" target="main"><?php echo $MSG_NEWS.$MSG_LIST?></a>
 <!-- slide相关功能暂时被弃用
-            <li><a href="slide_add_page.php" target="main"><b><?php echo "AddSlide" ?></b></a>
-            <li><a href="slide_list.php" target="main"><b><?php echo "slideList" ?></b></a>
+            <li><a href="slide_add_page.php" target="main"><?php echo "AddSlide" ?></a></li>
+            <li><a href="slide_list.php" target="main"><?php echo "slideList" ?></a></li>
 -->
             
-            <li><a href="rejudge.php" target="main"><b><?php echo $MSG_REJUDGE?></b></a></li>
+            <li><a href="rejudge.php" target="main"><?php echo $MSG_REJUDGE?></a></li>
         <?php 
           }
           if (isset($_SESSION['administrator'])){
         ?>
-            <li><a href="team_generate.php" target="main"><b><?php echo $MSG_TEAMGENERATOR?></b></a></li>
-            <!-- <li><a href="setmsg.php" target="main"><b><?php echo $MSG_SETMESSAGE?></b></a></li> -->
-            <li><a href="change_user_id.php" target="main"><b><?php echo "ChangeUserID" ?></b></a></li>
-            <li><a href="changepass.php" target="main"><b><?php echo $MSG_SETPASSWORD?></b></a></li>
-            <li><a href="privilege_add.php" target="main"><b><?php echo $MSG_ADD.$MSG_PRIVILEGE?></b></a></li>
-            <li><a href="privilege_list.php" target="main"><b><?php echo $MSG_PRIVILEGE.$MSG_LIST?></b></a></li>
-            <li><a href="source_give.php" target="main"><b><?php echo $MSG_GIVESOURCE?></b></a></li>
-            <li><a href="contestrank-solutions.php?cid=1000" target="main"><b><?php echo "ExportSource"?></b></a></li>
-            <li><a href="problem_export.php" target="main"><b><?php echo $MSG_EXPORT.$MSG_PROBLEM?></b></a></li>
-            <li><a href="problem_import.php" target="main"><b><?php echo $MSG_IMPORT.$MSG_PROBLEM?></b></a></li>
+            <li><a href="team_generate.php" target="main"><?php echo $MSG_TEAMGENERATOR?></a></li>
+            <!-- <li><a href="setmsg.php" target="main"><?php echo $MSG_SETMESSAGE?></a></li> -->
+            <li><a href="change_user_id.php" target="main"><?php echo "ChangeUserID" ?></a></li>
+            <li><a href="changepass.php" target="main"><?php echo $MSG_SETPASSWORD?></a></li>
+            <li><a href="privilege_add.php" target="main"><?php echo $MSG_ADD.$MSG_PRIVILEGE?></a></li>
+            <li><a href="privilege_list.php" target="main"><?php echo $MSG_PRIVILEGE.$MSG_LIST?></a></li>
+            <li><a href="source_give.php" target="main"><?php echo $MSG_GIVESOURCE?></a></li>
+            <li><a href="contestrank-solutions.php?cid=1000" target="main"><?php echo "ExportSource"?></a></li>
+            <li><a href="problem_export.php" target="main"><?php echo $MSG_EXPORT.$MSG_PROBLEM?></a></li>
+            <li><a href="problem_import.php" target="main"><?php echo $MSG_IMPORT.$MSG_PROBLEM?></a></li>
 
         <?php 
           }
@@ -76,14 +75,21 @@ if(isset($OJ_LANG)) require_once("../lang/$OJ_LANG.php");
           }
         ?>
             </ul>
-          </div>
         </center>
       </div>
 
       <div class="col-md-10 main">
-        <iframe name="main" src="watch.php" width="100%" height="100%" frameborder="0"></iframe>
+        <iframe name="main" src="watch.php" width="100%" height="90%" frameborder="0"></iframe>
       </div>
     </div>
   </div>
 </body>
 </html>
+<script>
+  $("li").click(function(){
+    $("li").each(function(){
+      $(this).removeClass("active");
+    });
+    $(this).addClass("active");
+  });
+</script>
