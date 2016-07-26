7ee24698738b65f3ec93c9a5760a0b02740bec34
OJ_server <wlx65005@163.com_OJ_server>
2016-07-26 17:33:04 +0800

    update .gitignore

--- web/OJ/upload/test.php
+++
@@ -1,8 +0,0 @@
-<?php
-$folder=上传的文件目录;
-$owner = posix_getpwuid(fileowner($folder));
-$group = posix_getpwuid(filegroup($folder));
-echo 'owner:'.$owner['name'].'<br>';
-echo 'group:'.$group['name'].'<br>';
-echo 'perms:'.substr(sprintf('%o',fileperms($folder)),-4);
-?>
