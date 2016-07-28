0164277fa1ad30b17befeaa176e955433157f58e
D_Star <wlx65005@163.com>
2016-07-28 07:51:04 -0700

    change the look of source page

--- web/OJ/template/hznu/showsource.php
+++ web/OJ/template/hznu/showsource.php
@@ -37,28 +37,47 @@
     </script>
     <?php
       if ($ok==true){
-        if($view_user_id!=$_SESSION['user_id'])
-          echo "<a href='mail.php?to_user=$view_user_id&title=$MSG_SUBMIT $id'>Mail the auther</a>";
+        $res_class="danger";
+        $time="-";
+        $memory="-";
+        if($sresult==4){//AC
+          $res_class="success";
+          $time=$stime;
+          $memory=$smemory;
+        }
+        else if($sresult<=3){//pending or rejudging or compling or judging
+          $res_class="default";
+        }
+
+echo <<<sss
+        <hr>
+        <div class='am-text-center'>
+          Problem_ID: <span class="am-badge am-badge-primary am-text-sm">
+                        <a href='/web/OJ/problem.php?id=$pid' style='color: white;'>$pid</a>
+                      </span>
+          Result: <span class="am-badge am-badge-$res_class am-text-sm">$judge_result[$sresult]</span>
+          Time: <span class="am-badge am-badge-warning am-text-sm">$time</span>
+          Memory: <span class="am-badge am-badge-warning am-text-sm">$memory</span>
+          Author: <span class="am-badge am-badge-secondary am-text-sm">
+                    <a href='/web/OJ/userinfo.php?user=$suser_id' style='color: white;'>$suser_id</a>
+                  </span>
+        </div>
+        <hr>
+sss;
+
+        // ****mail function currently stashed
+        // if($view_user_id!=$_SESSION['user_id'])
+        //   echo "<a href='mail.php?to_user=$view_user_id&title=$MSG_SUBMIT $id'>Mail the auther</a>";
         $brush=strtolower($language_name[$slanguage]);
         if ($brush=='pascal') $brush='delphi';
         if ($brush=='obj-c') $brush='c';
         if ($brush=='freebasic') $brush='vb';
         if ($brush=='swift') $brush='csharp';
         echo "<pre class=\"brush:".$brush.";\">";
-        ob_start();
-        echo "/**************************************************************\n";
-        echo "\tProblem: $sproblem_id\n\tUser: $suser_id\n";
-        echo "\tLanguage: ".$language_name[$slanguage]."\n\tResult: ".$judge_result[$sresult]."\n";
-        if ($sresult==4){
-          echo "\tTime:".$stime." ms\n";
-          echo "\tMemory:".$smemory." kb\n";
-        }
-        echo "****************************************************************/\n\n";
-        $auth=ob_get_contents();
-        ob_end_clean();
-        echo htmlentities(str_replace("\r\n","\n",$view_source),ENT_QUOTES,"utf-8")."\n".$auth."</pre>";
+        echo htmlentities(str_replace("\r\n","\n",$view_source),ENT_QUOTES,"utf-8");
+        echo "</pre>";
       } else {
-        echo "I am sorry, You could not view this code!";
+        echo "<div am-text-center><h2>I am sorry, You could not view this code!</h2></div>";
       }
     ?>
   </div>
