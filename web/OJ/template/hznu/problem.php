cba661649384b5c8b2064e14de7e040148f18f53
OJ_server <wlx65005@163.com>
2016-09-19 10:52:39 +0800

    add highlight to problem page

--- web/OJ/template/hznu/problem.php
+++ web/OJ/template/hznu/problem.php
@@ -230,3 +230,32 @@
     });
   });
 </script>
+<!-- highlight.js START-->
+<link href='highlight/styles/github-gist.css' rel='stylesheet' type='text/css'/>
+<script src='highlight/highlight.pack.js' type='text/javascript'></script>
+<script src='highlight/highlightjs-line-numbers.min.js' type='text/javascript'></script>
+<style type="text/css">
+  .hljs-line-numbers {
+      text-align: right;
+      border-right: 1px solid #ccc;
+      color: #999;
+      -webkit-touch-callout: none;
+      -webkit-user-select: none;
+      -khtml-user-select: none;
+      -moz-user-select: none;
+      -ms-user-select: none;
+      user-select: none;
+  }
+  code{
+    background: transparent;
+  }
+  pre.prettyprint{
+    background: transparent;
+  }
+</style>
+<script>
+  hljs.initHighlightingOnLoad();
+  hljs.initLineNumbersOnLoad();
+</script>
+
+<!-- highlight.js END-->
