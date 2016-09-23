7930988f6182bc5d9eb5f9a004a25f2729cc0e48
OJ_server <wlx65005@163.com>
2016-09-23 22:38:18 +0800

    bug fixed: a blank line before <code>

--- web/OJ/kindeditor/kindeditor.js
+++ web/OJ/kindeditor/kindeditor.js
@@ -17,7 +17,7 @@ if (!window.console) {
 if (!console.log) {
 	console.log = function () {};
 }
-var _VERSION = '4.1.11-modified',
+var _VERSION = '4.1.12-modified',
 	_ua = navigator.userAgent.toLowerCase(),
 	_IE = _ua.indexOf('msie') > -1 && _ua.indexOf('opera') == -1,
 	_NEWIE = _ua.indexOf('msie') == -1 && _ua.indexOf('trident') > -1,
