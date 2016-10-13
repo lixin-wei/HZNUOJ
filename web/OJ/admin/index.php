9816714082458886ce9709069a83e8ea153246e8
D_Star <wlx65005@163.com>
2016-10-13 06:55:04 -0700

    change the look of admin index, remove seeOJ page

--- web/OJ/admin/index.php
+++ web/OJ/admin/index.php
@@ -1,12 +1,14 @@
 <?php
 require_once("admin-header.php");
 if(isset($OJ_LANG)) require_once("../lang/$OJ_LANG.php");
+require_once "../include/my_func.inc.php";
 ?>
   <title>JudgeOnline Administration</title>
   <div class="container-fluid">
     <div class="row">
-        <h1>HZNUOJ</h1>
-        <h1>Administration Page</h1>
+    	<h1>Welcome To Administration Page</h1><hr>
+    	Your group is: <b><?php echo get_group($_SESSION['user_id']); ?></b><br>
+    	<a href="privilege_distribution.php">See what you can do here.</a>
     </div>
   </div>
 <?php 
