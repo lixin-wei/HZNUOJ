3b9a2f4f81bb5fae871f8fda470a77f81e99e89a
D_Star <wlx65005@163.com>
2016-11-10 03:55:54 -0800

    remove some useless code

--- web/OJ/template/hznu/contest.php
+++ web/OJ/template/hznu/contest.php
@@ -66,27 +66,4 @@
     </table>
   </div>
 </div>
-<script>
-var diff=new Date("<?php echo date("Y/m/d H:i:s")?>").getTime()-new Date().getTime();
-//alert(diff);
-function clock()
-    {
-      var x,h,m,s,n,xingqi,y,mon,d;
-      var x = new Date(new Date().getTime()+diff);
-      y = x.getYear()+1900;
-      if (y>3000) y-=1900;
-      mon = x.getMonth()+1;
-      d = x.getDate();
-      xingqi = x.getDay();
-      h=x.getHours();
-      m=x.getMinutes();
-      s=x.getSeconds();
-  
-      n=y+"-"+mon+"-"+d+" "+(h>=10?h:"0"+h)+":"+(m>=10?m:"0"+m)+":"+(s>=10?s:"0"+s);
-      //alert(n);
-      document.getElementById('nowdate').innerHTML=n;
-      setTimeout("clock()",1000);
-    } 
-    clock();
-</script>
 <?php include "footer.php" ?>
