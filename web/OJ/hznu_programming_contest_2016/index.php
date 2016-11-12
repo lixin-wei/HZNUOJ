62e09fd741ea86dd1eef7c0843806198920536d6
D_Star <wlx65005@163.com>
2016-11-12 01:25:29 -0800

    update the hznu contest page

--- web/OJ/hznu_programming_contest_2016/index.php
+++ web/OJ/hznu_programming_contest_2016/index.php
@@ -2,8 +2,150 @@
 $title="杭州师范大学程序设计竞赛";
 require_once "header.php";
 ?>
-<div class="am-container" style="padding-top: 20px;">
-	<h1>杭州师范大学程序设计竞赛</h1><hr>
-	报名页面制作中，预计14日之前开放，请随时关注。
+
+
+<style type="text/css" media="screen">
+	.big-title{
+		background: url("img/title_bk.png");
+		height: 300px;
+	}
+</style>
+<div class="big-title">
+	<div class="am-g am-text-center" style="padding-top: 75px;">
+		<span style="font-size: 30pt;">第十届杭州师范大学程序设计竞赛</span>
+	</div>
+	<div class="am-g" style="margin-top: 60px; width: 70%;">
+		<div class="am-u-sm-12">
+			<ul class="am-nav am-nav-pills am-nav-justify">
+			  <li class="am-active"><a href="#">通知与注意事项</a></li>
+			  <li><a href="register.php">注册与报名</a></li>
+			</ul>
+		</div>
+	</div>
 </div>
+
+<div class="am-container" style="padding-top: 20px;">
+<h1>特别提醒</h1>
+<p style="padding-left: 30px;">请务必使用chrome或者Firefox浏览器浏览本页面，以免由于兼容性引发不必要的错误。包括但不限于页面排版错乱、注册失败等。</p>
+<p style="padding-left: 30px;">所有的比赛相关信息都会在本页面第一时间公布与更新，请随时留意。</p>
+<h1>报名方式</h1>
+<p style="padding-left: 30px;">比赛采取网上报名。</p>
+<p style="padding-left: 30px;">报名页面正在制作中，预计11月14日开放。</p>
+<p style="padding-left: 30px;">请点击上方导航栏中的&ldquo;注册与报名&rdquo;一栏，前往报名页面进行报名。</p>
+<p style="padding-left: 30px;">报名必填的信息有：学院、班级、学号、姓名，你在报名时可以选择匿名，这样你的信息将不会被显示在已报名列表里。</p>
+<p style="padding-left: 30px;">报名成功后，可点击咨询按钮咨询自己是否已经报名。</p>
+<p style="padding-left: 30px;">报名成功即不可撤销，请谨慎考虑。</p>
+<p style="padding-left: 30px;">请不要重复报名，以免造成信息错乱，影响你的报名。</p>
+<h1>赛程安排</h1>
+<p style="padding-left: 30px;">2016年11月30日（星期三）下午24：00前报名参赛；</p>
+<p style="padding-left: 30px;">2016年12月10日（星期六）上午9：00-11：00进行机房布置与试机准备，下午12：00-17：00于恕园16-302举行杭州师范大学第十届大学生程序设计竞赛现场赛。</p>
+<p style="padding-left: 30px;">2016年12月10日晚上19：00进行滚榜、公布最终榜单、举行颁奖仪式暨赛后总结。</p>
+<h1>比赛规则</h1>
+<ol>
+<li>参赛队员可以携带书，手稿，程序清单等纸质参考资料以及水笔。</li>
+<li>参赛队员不可携带任何可用于计算机处理的软件或数据（不允许携带任何私人U盘，电脑等）。</li>
+<li>参赛队员不能携带任何类型通讯工具，包括手机，传呼机等。</li>
+<li>赛场保证绝对安静整洁，若有大声喧哗，不服从工作人员管理，干扰他人比赛者，都将剥夺其比赛资格。</li>
+<li>赛场严禁参赛人员间交头接耳，杜绝代码雷同，团伙作弊等现象。一经发现将取消其比赛资格，并根据相关文件给于对应处罚。</li>
+</ol>
+<h1>赛制</h1>
+<p style="padding-left: 30px;">本次比赛采用ACM/ICPC赛制。</p>
+<p style="padding-left: 30px;">比赛平台：HZNUOJ（acm.hznu.edu.cn），可以注册个号早点熟悉下。</p>
+<p style="padding-left: 30px;">题目数：8~11题（未定），难度较高。</p>
+<p style="padding-left: 30px;">可用程序语言：C、C++、Bash、C#、Lua、Pascal、Perl、Python2、Ruby。</p>
+<p style="padding-left: 30px;">但我们不保证题目用C/C++以外语言的可解性。</p>
+<p style="padding-left: 30px;"><strong>详细评分机制：</strong></p>
+<p style="padding-left: 60px;">试题解答将在HZNUOJ上直接提交，并且每一次运行会被评判正确或错误，评判结果可由参赛者自主查询。HZNUOJ同时设有代码检查机制，若代码一模一样，则视为作弊，将做严肃处理。</p>
+<p style="padding-left: 60px;">个人解答数居中等及中等以上的参赛者将会根据相应解答数进行排名，解答数在中等数量以下的队伍会确认解答数但不进行排名。在判定获奖环节时，如果多位参赛者解题数相同，则根据总用时加上罚时进行排名。总用时与罚时总和较小者名次靠前。总用时与惩罚时间由每道解答正确的试题的用时加上惩罚时间而成。每道试题用时从比赛开始到该试题解答被判定通过为止，每一次错误提交将被加罚20分钟，未正确解答的试题不记时。</p>
+<p style="padding-left: 60px;">比赛最后一个小时将进行封榜，所有提交结果以未知的灰色表示，仅提交者本人能看到结果，最终榜单会在滚榜阶段公布。</p>
+<p style="padding-left: 30px;"><strong>滚榜机制介绍：</strong></p>
+<p style="padding-left: 60px;">滚榜是ACM/ICPC系列比赛中一个十分具有特色的机制。</p>
+<p style="padding-left: 60px;">在正规ACM/ICPC系列比赛中，比赛最后一个小时的提交结果是隐藏的，只有选手本人能看到，在榜单上会显示成代表未知的灰色，以增加比赛紧张气氛。</p>
+<p style="padding-left: 60px;">然后在颁奖会上，将从榜单最后一名开始，一个个揭晓灰色的未知提交，一旦揭晓的结果为通过，这个人的排名就会上升，否则这个人的排名确定，开始揭晓下一个人，以此类推。这样一来，可以从后往前一个个确定最终排名，一旦名次达到获奖名次内，可以直接进行颁奖。整个过程惊险刺激，是整个比赛的亮点所在。</p>
+<h1>奖项设置（暂定）</h1>
+<p style="padding-left: 30px;">一等奖：5%</p>
+<p style="padding-left: 30px;">二等奖：15%</p>
+<p style="padding-left: 30px;">三等奖：30%</p>
+<p style="padding-left: 30px;">最佳女生奖：排名最高的女生</p>
+<p style="padding-left: 30px;">顽强拼搏奖：全场比赛最后一个通过题目的人。</p>
+<h1>比赛环境</h1>
+<p style="padding-left: 30px;"><strong>测评环境：</strong></p>
+<div style="padding-left: 60px;">
+<p>
+    OS: <a href="http://www.ubuntu.com">Ubuntu 14.04</a><br>
+      
+  <!-- 编译器选项表格 start -->
+  <table class="am-table am-table-bordered am-table-striped am-table-compact">
+    <tr>
+      <td><center>Languages</center></td>
+      <td><center>Compiler</center></td>
+      <td><center>Cmd</center></td>
+    </tr>
+    <tr>
+      <td><center>C</center></td>
+      <td><center>gcc 4.8.2</center></td>
+      <td><center><font color=blue>gcc Main.c -o Main  -fno-asm -O2 -Wall -lm --static -std=c99 -DONLINE_JUDGE</font></center></td>
+    </tr>
+    <tr>
+      <td><center>C++</center></td>
+      <td><center>g++ 4.8.2</center></td>
+      <td><center><font color=blue>g++ Main.cc -o Main  -fno-asm -O2 -Wall -lm --static  -std=c++11 -DONLINE_JUDGE</font></center></td>
+    </tr>
+    <tr>
+      <td><center>Pascal</center></td>
+      <td><center>Free Pascal 2.6.2</center></td>
+      <td><center><font color=blue>fpc Main.pas -oMain -O1 -Co -Cr -Ct -Ci </font></center></td>
+    </tr>
+    <tr>
+      <td><center>Java</td>
+      <td><center>openjdk 1.7.0_79</center></td>
+      <td><center><font color="blue">javac -J-Xms32m -J-Xmx256m Main.java</font>
+      <br>
+      <font size="-1" color="red">Languages except C/C++ has 2 more seconds and 128M more memory when running and judging.</font></center>
+      </td>
+    </tr>
+    <tr>
+      <td><center>Ruby</center></td>
+      <td><center>1.9.3</center></td>
+      <td><center></center></td>
+    </tr>
+    <tr>
+      <td><center>Bash</center></td>
+      <td><center>4.3.11</center></td>
+      <td><center></center></td>
+    </tr>
+    <tr>
+      <td><center>Python</center></td>
+      <td><center>2.7.6</center></td>
+      <td><center></center></td>
+    </tr>
+    <tr>
+      <td><center>PHP</center></td>
+      <td><center>5.5.9</center></td>
+      <td><center></center></td>
+    </tr>
+    <tr>
+      <td><center>Perl</center></td>
+      <td><center>perl 5 version 18</center></td>
+      <td><center></center></td>
+    </tr>
+    <tr>
+      <td><center>C#</center></td>
+      <td><center>mono 3.2.8</center></td>
+      <td><center></center></td>
+    </tr>
+    <tr>
+      <td><center>Lua</center></td>
+      <td><center>5.2.3</center></td>
+      <td><center></center></td>
+    </tr>
+  </table>
+</div>
+<p style="padding-left: 30px;"><strong>比赛环境：</strong></p>
+<p style="padding-left: 60px;">OS: &nbsp;windows7</p>
+<p style="padding-left: 60px;">IDE: VS2010</p>
+</div>
+
+
+
 <?php require_once "../template/hznu/footer.php" ?>
---
+++ web/OJ/hznu_programming_contest_2016/register.php
@@ -0,0 +1,28 @@
+<?php
+$title="杭州师范大学程序设计竞赛";
+require_once "header.php";
+?>
+<style type="text/css" media="screen">
+	.big-title{
+		background: url("img/title_bk.png");
+		height: 300px;
+	}
+</style>
+<div class="big-title">
+	<div class="am-g am-text-center" style="padding-top: 75px;">
+		<span style="font-size: 30pt;">第十届杭州师范大学程序设计竞赛</span>
+	</div>
+	<div class="am-g" style="margin-top: 60px; width: 70%;">
+		<div class="am-u-sm-12">
+			<ul class="am-nav am-nav-pills am-nav-justify">
+			  <li><a href="index.php">通知与注意事项</a></li>
+			  <li class="am-active"><a href="register.php">注册与报名</a></li>
+			</ul>
+		</div>
+	</div>
+</div>
+<div class="am-container" style="padding-top: 20px;">
+<h1>公告</h1>
+报名页面正在制作中，预计11月14日开放。
+</div>
+<?php require_once "../template/hznu/footer.php" ?>
