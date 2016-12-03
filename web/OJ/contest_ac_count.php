faa1f55abaa15cf3dac9c1713a5b2dd406c78777
D_Star <wlx65005@163.com>
2016-12-03 11:29:30 -0800

    create contest_ac_count.php

---
+++ web/OJ/contest_ac_count.php
@@ -0,0 +1,34 @@
+<?php
+if(isset($_GET['cid'])){
+	require_once $_SERVER['DOCUMENT_ROOT']."/OJ/template/hznu/header.php";
+	echo "<div class='am-container' style='max-width:500px;'>";
+	require_once("./include/const.inc.php");
+	$cid=$_GET['cid'];
+	$sql=<<<SQL
+		SELECT
+			team.nick,
+			solution.num
+		FROM
+			solution
+		LEFT JOIN team ON solution.user_id = team.user_id
+		WHERE solution.contest_id=$cid 
+		ORDER BY solution.in_date DESC
+SQL;
+	$res=$mysqli->query($sql);
+	$cnt=$res->num_rows;
+	echo "<table class='am-table am-table-striped am-table-hover'>";
+	while($row=$res->fetch_object()){
+		echo <<<HTML
+			<tr>
+				<td>$cnt</td>
+				<td>{$PID[$row->num]}</td>
+				<td>$row->nick</td>
+			</tr>
+HTML;
+		$cnt--;
+	}
+	echo "</table>";
+	echo "</div>";
+	require_once $_SERVER['DOCUMENT_ROOT']."/OJ/template/hznu/footer.php";
+}
+?>
