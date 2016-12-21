642d8f30572617d702ea93b0a04e4855d2104be8
D_Star <wlx65005@163.com>
2016-12-21 08:07:12 -0800

    add hit times to index chart

--- web/OJ/include/db_info.inc.php
+++ web/OJ/include/db_info.inc.php
@@ -1,47 +1,52 @@
 <?php
-  /**
-   * This file is modified
-   * by yybird @2016.05.25
-   * by D_Star @2016.08.23
-  **/
+/**
+ * This file is modified
+ * by yybird @2016.05.25
+ * by D_Star @2016.08.23
+ **/
 ?>
 <?php @session_start();
 
-  ini_set("display_errors","Off");
-  require_once($_SERVER['DOCUMENT_ROOT']."/OJ/include/static.php");
+ini_set("display_errors","Off");
+require_once($_SERVER['DOCUMENT_ROOT']."/OJ/include/static.php");
 
-  //if(date('H')<5||date('H')>21||isset($_GET['dark'])) $OJ_CSS="dark.css";
-  if (isset($_SESSION['OJ_LANG'])) $OJ_LANG=$_SESSION['OJ_LANG'];
-  
-  //for normal install
-  $mysqli=new mysqli($DB_HOST,$DB_USER,$DB_PASS,$DB_NAME);
-  if($mysqli->connect_errno) 
+//if(date('H')<5||date('H')>21||isset($_GET['dark'])) $OJ_CSS="dark.css";
+if (isset($_SESSION['OJ_LANG'])) $OJ_LANG=$_SESSION['OJ_LANG'];
+
+//for normal install
+$mysqli=new mysqli($DB_HOST,$DB_USER,$DB_PASS,$DB_NAME);
+if($mysqli->connect_errno)
     die('Could not connect: ' . $mysqli->connect_error);
-
-  // use db
-  $mysqli->query("set names utf8");
-  //if(!$OJ_SAE)mysqli_set_charset("utf8");
-  //sychronize php and mysql server
-  date_default_timezone_set("PRC");
-  $mysqli->query("SET time_zone ='+8:00'");
-  // 管理权限
-  function HAS_PRI($pri_str){  // if has privilege
+// use db
+$mysqli->query("set names utf8");
+//if(!$OJ_SAE)mysqli_set_charset("utf8");
+//sychronize php and mysql server
+date_default_timezone_set("PRC");
+$mysqli->query("SET time_zone ='+8:00'");
+// 管理权限
+function HAS_PRI($pri_str){  // if has privilege
     //non-realtime
     //return $_SESSION[$pri_str];
     
     //realtime checking
     global $mysqli;
     if(isset($_SESSION['user_id'])){
-      $sql="SELECT `rightstr` FROM `privilege` WHERE `user_id`='".$mysqli->real_escape_string($_SESSION['user_id'])."'";
-      $res=$mysqli->query($sql);
-      while($group_name=$res->fetch_array()['rightstr']){
-        $rs=$mysqli->query("SELECT * FROM privilege_distribution WHERE group_name='$group_name'");
-        $arr=$rs->fetch_array();
-        if($arr[$pri_str]){
-          return true;
+        $sql="SELECT `rightstr` FROM `privilege` WHERE `user_id`='".$mysqli->real_escape_string($_SESSION['user_id'])."'";
+        $res=$mysqli->query($sql);
+        while($group_name=$res->fetch_array()['rightstr']){
+            $rs=$mysqli->query("SELECT * FROM privilege_distribution WHERE group_name='$group_name'");
+            $arr=$rs->fetch_array();
+            if($arr[$pri_str]){
+                return true;
+            }
         }
-      }
     }
     return false;
-  }
+}
+/*Count the hit time START*/
+$user_id="";
+if(isset($_SESSION['user_id'])) $user_id=$_SESSION['user_id'];
+$sql="INSERT INTO hit_log (ip, time, path, user_id) VALUES ('{$_SERVER['REMOTE_ADDR']}', NOW(), '{$_SERVER['REQUEST_URI']}', '$user_id')";
+$mysqli->query($sql);
+/*Count the hit time END*/
 ?>
--- web/OJ/template/hznu/index.php
+++ web/OJ/template/hznu/index.php
@@ -1,21 +1,21 @@
 <?php
-  /**
-   * This file is created
-   * by yybird
-   * @2016.03.22
-   * last modified
-   * by yybird
-   * @2016.05.07
-   * last modified
-   * by wlx
-   * @2016.05.24
-  **/
+/**
+ * This file is created
+ * by yybird
+ * @2016.03.22
+ * last modified
+ * by yybird
+ * @2016.05.07
+ * last modified
+ * by wlx
+ * @2016.05.24
+ **/
 ?>
 
 <?php $title="Home";?>
-<?php 
-  require_once("header.php");
-  require_once('./include/db_info.inc.php');
+<?php
+require_once("header.php");
+require_once('./include/db_info.inc.php');
 ?>
 
 <style>
@@ -44,18 +44,18 @@
         <div class="am-panel-hd" class="am-panel-title">News</div>
         <div class="am-panel-collapse am-collapse am-in">
           <div class="am-panel-bd">
-            <div class="am-panel-group" id="accordion"> 
-              <?php
+            <div class="am-panel-group" id="accordion">
+                <?php
                 $n = count($news_title);
                 if ($n) { // 有公告的话
-                  for ($i=0; $i<$n; ++$i) {
-                    $nid=$news_id[$i];
-                    if ($news_importance[$i] == 10) echo "<div class='am-panel am-panel-danger'>";
-                    else if ($news_importance[$i] == 3) echo "<div class='am-panel am-panel-warning'>";
-                    else if ($news_importance[$i] == 2) echo "<div class='am-panel am-panel-secondary'>";
-                    else if ($news_importance[$i] == 1) echo "<div class='am-panel am-panel-success'>";
-                    else echo "<div class='am-panel am-panel-default'>";
-echo <<<HTML
+                    for ($i=0; $i<$n; ++$i) {
+                        $nid=$news_id[$i];
+                        if ($news_importance[$i] == 10) echo "<div class='am-panel am-panel-danger'>";
+                        else if ($news_importance[$i] == 3) echo "<div class='am-panel am-panel-warning'>";
+                        else if ($news_importance[$i] == 2) echo "<div class='am-panel am-panel-secondary'>";
+                        else if ($news_importance[$i] == 1) echo "<div class='am-panel am-panel-success'>";
+                        else echo "<div class='am-panel am-panel-default'>";
+                        echo <<<HTML
                       <div class="am-panel-hd" class="am-panel-title" data-am-collapse="{target: '#news-$nid'}">
                         $news_title[$i]
                       </div>
@@ -65,24 +65,24 @@ echo <<<HTML
                       </div>    
                     </div>  
 HTML;
-                  }
+                    }
                 }
-              ?>    
+                ?>
             </div>
           </div>
         </div>
       </div>
     </div>
     <!-- 公告模块 end -->
-
-
+    
+    
     <!--Submission Statics START-->
     <div class="am-u-md-4">
-      <?php
-      $sql="SELECT count(*) FROM solution WHERE in_date<=NOW() AND in_date>date_add(NOW(), interval -1 day)";
-      $res=$mysqli->query($sql);
-      $cnt=$res->fetch_array()[0];
-      ?>
+        <?php
+        $sql="SELECT count(*) FROM solution WHERE in_date<=NOW() AND in_date>date_add(NOW(), interval -1 day)";
+        $res=$mysqli->query($sql);
+        $cnt=$res->fetch_array()[0];
+        ?>
       <div class="am-panel am-panel-primary">
         <div class="am-panel-hd">Statics</div>
         <div id="submission_chart" style="width: 100%;height: 250px;"></div>
@@ -90,19 +90,22 @@ HTML;
     </div>
     <!--Submission Statics END-->
   </div>
-  
-  
-  </div>
+
+
+</div>
 
 </div>
 <?php require_once("footer.php") ?>
 
 <script type="text/javascript">
-  $('div.link').click(function(){
-    window.open('http://www.pixiv.net/member_illust.php?mode=medium&illust_id=13212258');
-    //window.location.href="http://pixiv.net/member.php?id=430651";
-  });
+    $('div.link').click(function(){
+        window.open('http://www.pixiv.net/member_illust.php?mode=medium&illust_id=13212258');
+        //window.location.href="http://pixiv.net/member.php?id=430651";
+    });
 </script>
+
+
+
 <script type="text/javascript" src="/OJ/plugins/echarts/echarts.min.js"></script>
 <!-- <script src="//cdn.bootcss.com/echarts/3.2.3/echarts.min.js"></script> -->
 <script type="text/javascript">
@@ -112,129 +115,146 @@ HTML;
 </script>
 
 
-<!--get recent submission json START-->
+<!--get charts json START-->
 <?php
 $tot_days=20;
 $series_not_ac_data="";
 $series_ac_data="";
+$series_hits_data="";
 $xAxis_data="";
 for($i=$tot_days-1 ; $i>=0 ; --$i){
-
-
-  $sql="SELECT count(*) FROM solution WHERE in_date<=date_add(date(NOW()), interval -$i+1 day) AND in_date>date_add(date(NOW()), interval -$i day) AND result != 4";
-  $res=$mysqli->query($sql);
-  $cnt=$res->fetch_array()[0];
-  $series_not_ac_data.="$cnt,";
-
-  $sql="SELECT count(*) FROM solution WHERE in_date<=date_add(date(NOW()), interval -$i+1 day) AND in_date>date_add(date(NOW()), interval -$i day) AND result = 4";
-  $res=$mysqli->query($sql);
-  $cnt=$res->fetch_array()[0];
-  $series_ac_data.="$cnt,";
-
-
-  if($i==0)$xAxis_data.="'today',";
-  else if($i==1) $xAxis_data.="'yesterday',";
-  else $xAxis_data.="'".(-$i+1)." days',";
+    
+    
+    $sql="SELECT count(1) FROM solution WHERE in_date<=date_add(date(NOW()), interval -$i+1 day) AND in_date>date_add(date(NOW()), interval -$i day) AND result != 4";
+    $res=$mysqli->query($sql);
+    $cnt=$res->fetch_array()[0];
+    $series_not_ac_data.="$cnt,";
+    
+    $sql="SELECT count(1) FROM solution WHERE in_date<=date_add(date(NOW()), interval -$i+1 day) AND in_date>date_add(date(NOW()), interval -$i day) AND result = 4";
+    $res=$mysqli->query($sql);
+    $cnt=$res->fetch_array()[0];
+    $series_ac_data.="$cnt,";
+    
+    $sql="SELECT count(1) FROM hit_log WHERE time<=date_add(date(NOW()), interval -$i+1 day) AND time>date_add(date(NOW()), interval -$i day)";
+    $res=$mysqli->query($sql);
+    $cnt=$res->fetch_array()[0];
+    $series_hits_data.="$cnt,";
+    
+    if($i==0)$xAxis_data.="'today',";
+    else if($i==1) $xAxis_data.="'yesterday',";
+    else $xAxis_data.="'".(-$i+1)." days',";
 }
 ?>
-<!--get recent submission json END-->
+<!--get charts json END-->
 
 
 <script type="text/javascript">
-$(document).ready(function(){
-    // 指定图表的配置项和数据
-    var option = {
-        grid: {
-            x: 65,
-        },
-        color: ['#3398DB','#5EB95E'],
-        title: {
-            text: 'Submissions In Recent Days',
-            padding: 15,
-        },
-        tooltip: {
-            trigger: 'axis',
-            axisPointer: {
-                type: 'shadow',
+    $(document).ready(function(){
+        // 指定图表的配置项和数据
+        var option_submission = {
+            grid: {
+                x: 65
             },
-            formatter : function(params){
-                var tot=params[0].data+params[1].data;
-                var ac=params[1].data;
-                return "AC: "+ac+"<br>Total: "+tot;
+            color: ['#3398DB','#5EB95E','#F37B49'],
+            tooltip: {
+                trigger: 'axis',
+                axisPointer: {
+                    type: 'shadow'
+                },
+                formatter : function(params){
+                    var tot=params[0].data+params[1].data;
+                    var ac=params[1].data;
+                    var hits=params[2].data;
+                    return "AC: "+ac+"<br>Total: "+tot+"<br/>Hits: "+hits;
+                }
             },
-        },
-        legend: {
-            show: false,
-            data:['submissions'],
-        },
-        xAxis: {
-            type: 'category',
-            data: [<?php echo $xAxis_data; ?>],
-        },
-        yAxis: {},
-        series: [
-            {
-                name: 'submissions',
-                type: 'bar',
-                stack: 'total',
-                data: [<?php echo $series_not_ac_data; ?>],
+            legend: {
+                show: false,
+                data:['submissions']
             },
-            {
-                name: 'AC',
-                type: 'bar',
-                stack: 'total',
-                data: [<?php echo $series_ac_data; ?>],
+            xAxis: {
+                type: 'category',
+                data: [<?php echo $xAxis_data; ?>]
             },
-        ]
-    };
-
-    // 使用刚指定的配置项和数据显示图表。
-    submission_chart.setOption(option);
-    submission_chart.hideLoading();
-});
-$(window).resize(function(){
-  submission_chart.resize();
-});
-$(window).ready(function(){
-  submission_chart.resize();
-});
+            yAxis: [
+                {
+                    type : "value",
+                    name : "Submissions"
+                },
+                {
+                    splitLine: false,
+                    type : "value",
+                    name : "Hits"
+                }
+            ],
+            series: [
+                {
+                    name: 'Submissions',
+                    type: 'bar',
+                    stack: 'total',
+                    data: [<?php echo $series_not_ac_data; ?>]
+                },
+                {
+                    name: 'AC',
+                    type: 'bar',
+                    stack: 'total',
+                    data: [<?php echo $series_ac_data; ?>]
+                },
+                {
+                    name: 'Hits',
+                    type: 'line',
+                    yAxisIndex: 1,
+                    data: [<?php echo $series_hits_data; ?>]
+                }
+            ]
+        };
+        // 使用刚指定的配置项和数据显示图表。
+        submission_chart.setOption(option_submission);
+        submission_chart.hideLoading();
+    });
+    $(window).resize(function(){
+        submission_chart.resize();
+    });
+    $(window).ready(function(){
+        submission_chart.resize();
+    });
 </script>
 
 <script type="text/javascript">
-  var has_load=Array();
-  $("div[id^='news-']").on('open.collapse.amui', function() {
+    var has_load=[];
+    $("div[id^='news-']").on('open.collapse.amui', function() {
 
-    var id=$(this).attr("id");
-    var news_id=id.substring(5);
-    if(has_load[news_id])return;
+        var id=$(this).attr("id");
+        var news_id=id.substring(5);
+        if(has_load[news_id])return;
 
-    //begin load
-    $("i#news-load-icon-"+news_id).show(0);
-    $.when($.ajax({
-      type: "GET",
-      url: "get_news.php",
-      data: {
-        id: news_id,
-      },
-      context: this,
-      success: function(data){
-        $(this).find("div.am-panel-bd").html(data);
-        has_load[news_id]=true;
-        //console.log($(this).attr("id"));
-      },
-      complete: function(){
-        console.log("ajax complete!");
-      },
-      error: function(xmlrqst,info){
-        console.log(info);
-      }
-    })).done(function(){
-      $("i#news-load-icon-"+news_id).hide(0);
+        //begin load
+        $("i#news-load-icon-"+news_id).show(0);
+        $.when($.ajax({
+            type: "GET",
+            url: "get_news.php",
+            data: {
+                id: news_id
+            },
+            context: this,
+            success: function(data){
+                $(this).find("div.am-panel-bd").html(data);
+                has_load[news_id]=true;
+                //console.log($(this).attr("id"));
+            },
+            complete: function(){
+                console.log("ajax complete!");
+            },
+            error: function(xmlrqst,info){
+                console.log(info);
+            }
+        })).done(function(){
+            $("i#news-load-icon-"+news_id).hide(0);
+        });
+
+    }).on('close.collapse.amui', function() {
+        console.log('折叠菜单关闭鸟！');
     });
-
-  }).on('close.collapse.amui', function() {
-    console.log('折叠菜单关闭鸟！');
-  });
 </script>
 <!-- modal auto jump START -->
 <!-- <div class="am-modal am-modal-no-btn" tabindex="-1" id="index_ad_modal">
