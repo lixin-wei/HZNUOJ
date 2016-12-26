2df22dcfb1b97bd1d90c052989a571832cd5a5fa
D_Star <wlx65005@163.com>
2016-12-26 00:34:56 -0800

    revert judged to old version

---
+++ judger/core/judged/cmake-build-debug/CMakeFiles/judged.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/judged.dir/judged.cc.o"
+  "judged.pdb"
+  "judged"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/judged.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
