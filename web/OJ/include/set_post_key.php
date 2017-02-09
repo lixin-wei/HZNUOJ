e5db6f23862b920bc22c12cf73649b8f33d704d1
D_Star <wlx65005@163.com>
2017-02-09 01:26:49 -0800

    change random_bytes to rand for php5

--- web/OJ/include/set_post_key.php
+++ web/OJ/include/set_post_key.php
@@ -1,7 +1,7 @@
 <?php @session_start();
 
 if (!isset($_SESSION['csrf_token'])) {
-    $_SESSION['csrf_token'] = MD5(random_bytes(10));
+    $_SESSION['csrf_token'] = MD5(rand(0,999999)."asdjlkajs@@!#@!#");
 }
 ?>
 <input type=hidden name="csrf_token" value="<?php echo $_SESSION['csrf_token']?>">
