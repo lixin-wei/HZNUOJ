36980abcb103b22ca7d3c37386d84df050d23569
D_Star <wlx65005@163.com>
2017-09-20 05:11:02 -0700

    source compare fixed

--- web/OJ/template/hznu/comparesource.php
+++ web/OJ/template/hznu/comparesource.php
@@ -12,42 +12,54 @@
 <?php 
   $title="Compare Source Code";
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title></title>
+  <link type="text/css" rel="stylesheet" href="plugins/mergely/mergely.css" />
+</head>
+<body>
 <div class="am-container">
   <div class="am-jumbotron">
     <div id='main'>
-      <!-- Requires jQuery -->
-      <script language="javascript" type="text/javascript" src="include/jquery-latest.js"></script> 
-      <!-- Requires CodeMirror 2.16 -->
-      <script type="text/javascript" src="mergely/codemirror.js"></script>
-      <link type="text/css" rel="stylesheet" href="mergely/codemirror.css" />
-      <!-- Requires Mergely -->
-      <script type="text/javascript" src="mergely/mergely.js"></script>
-      <link type="text/css" rel="stylesheet" href="mergely/mergely.css" />
-      <script type="text/javascript">
-        $(document).ready(function () {
-          $('#compare').mergely({
-            cmsettings: { readOnly: false, lineWrapping: true }
-          });
-          $.ajax({
-            type: 'GET', async: true, dataType: 'text',
-            url: 'getsource.php?id=<?php echo intval($_GET['left'])?>',
-            success: function (response) {
-              $('#compare').mergely('lhs', response);
-            }
-          });
-          $.ajax({
-            type: 'GET', async: true, dataType: 'text',
-            url: 'getsource.php?id=<?php echo intval($_GET['right'])?>',
-            success: function (response) {
-              $('#compare').mergely('rhs', response);
-            }
-          });
-        });
-      </script>
-      <div id="mergely-resizer">
         <div id="compare" >
-      </div>
-    </div>
-  </div><!--end main-->
-</div><!--end wrapper-->
+        </div>
+    </div><!--end main-->
+  </div><!--end wrapper-->
+  <!-- Requires jQuery -->
+  <script language="javascript" type="text/javascript" src="plugins/jquery/jquery-1.9.0.min.js"></script>
+  <!-- Requires CodeMirror 2.16 -->
+  <script type="text/javascript" src="plugins/mergely/codemirror.js"></script>
+  <link type="text/css" rel="stylesheet" href="plugins/mergely/codemirror.css" />
+  <!-- Requires Mergely -->
+  <script type="text/javascript" src="plugins/mergely/mergely.js"></script>
+  <script type="text/javascript">
+      $(document).ready(function () {
+          $('#compare').mergely({
+              cmsettings: {
+                  readOnly: false,
+                  lineWrapping: true,
+              }
+          });
+          $.ajax({
+              type: 'GET', async: true, dataType: 'text',
+              url: 'getsource.php?id=<?php echo intval($_GET['left'])?>',
+              success: function (response) {
+                  $('#compare').mergely('lhs', response);
+              }
+          });
+          $.ajax({
+              type: 'GET', async: true, dataType: 'text',
+              url: 'getsource.php?id=<?php echo intval($_GET['right'])?>',
+              success: function (response) {
+                  $('#compare').mergely('rhs', response);
+              }
+          });
+      });
+  </script>
+</body>
+</html>
 
