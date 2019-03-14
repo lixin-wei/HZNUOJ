c231ee6fe96029061c209f7bb909b16ac44ccd87
jnxxhzz <33682882+jnxxhzz@users.noreply.github.com>
2019-03-15 02:03:53 +0800

    实现了插入与读取数据库的功能，需要优化界面
    实现了插入与读取数据库的功能，需要优化界面

--- web/OJ/template/hznu/contest_code_printer.php
+++ web/OJ/template/hznu/contest_code_printer.php
@@ -1,12 +1,66 @@
+<?php
+  /**
+   * Need to modify the interface
+   * by jnxxhzz
+   * @2019.03.15
+  **/
+?>
 <?php include "template/hznu/contest_header.php"; ?>
 <div class="am-container">
     <h2>Code Submiting History</h2>
     <hr />
-    <ul class="am-list am-list-static am-list-border">
-      <li>
-        <span class="am-badge am-badge-success">Printed</span> <span class="am-badge">Pending</span>
-        每个人都有一个死角， 自己走不出来，别人也闯不进去。</li>
-    </ul>
-    <button class="am-btn am-btn-primary am-btn-lg am-btn-block">Upload Code</button>
+    <form method="post">
+    	<div class="form-group">
+        <label class="col-sm-2 control-label">Description</label>
+        <div class="col-sm-10"><textarea name="code_print" id="" rows="13" class="kindeditor"></textarea></div>
+      </div>
+    <button type="submit" value="submit" class="btn btn-default">Submit</button>
+	</form> 
+    <?php
+    $user_id=$mysqli->real_escape_string($_SESSION['user_id']);
+    $contest_id=$mysqli->real_escape_string($_GET['cid']);
+    $sql = "select code,in_date from printer_code where user_id = '$user_id' and contest_id = $contest_id";
+    $result = $mysqli->query ( $sql );
+ 	if ($result){
+	 	while ($row=$result->fetch_object()) {
+	 		$code_write = $row->code;
+	 		$code_len = strlen($code_write)."B";
+	 		if(strlen($code_write) >= 150) $code_write = substr($code_write,0,150)."...";
+	 		$code_write = htmlentities($code_write,ENT_QUOTES,"UTF-8");
+
+	 		echo <<<HTML
+	 		<ul class="am-list am-list-static am-list-border">
+		      <li>
+		        <span class="am-badge am-badge-success">$code_len</span> <span class="am-badge">$row->in_date;</span>
+		        
+		        $code_write
+		        </li></ul>
+HTML;
+
+	 	}
+	 }
+	 $result->free();
+
+?>
+    
 </div>
+
+
+
 <?php include "footer.php" ?>
+
+<?php
+
+ 
+if(isset($_POST['code_print'])){
+    $user_id=$mysqli->real_escape_string($_SESSION['user_id']);
+    $contest_id=$mysqli->real_escape_string($_GET['cid']);
+    $code=$mysqli->real_escape_string($_POST ['code_print']);
+  //  $spj=($spj);
+    $sql = "INSERT into `printer_code` (`user_id`,`contest_id`,`code`,`in_date`) VALUES('$user_id','$contest_id','$code',NOW())";
+    @$mysqli->query ( $sql ) or die ( $mysqli->error );
+ }
+
+?>
+
+
