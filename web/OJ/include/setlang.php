f53aa080b7d06f698c6e00c6a38421f971cdb6a1
lixun2015 <lixun516@qq.com>
2020-01-11 23:38:00 +0800

    使用语言文件的绝对路径

--- web/OJ/include/setlang.php
+++ web/OJ/include/setlang.php
@@ -8,11 +8,11 @@
 
 <?php 
   if (isset($OJ_LANG)) {
-    require_once("./lang/$OJ_LANG.php");
+    require_once($_SERVER['DOCUMENT_ROOT']."/OJ/lang/$OJ_LANG.php");
     if (file_exists("./faqs.$OJ_LANG.php")) {
       $OJ_FAQ_LINK = "./faqs.$OJ_LANG.php";
     }
   } else {
-    require_once("./lang/en.php");
+    require_once($_SERVER['DOCUMENT_ROOT']."/OJ/lang/en.php");
   }
 ?>
