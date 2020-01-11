2a26b736c5f75caf3b41cbb613b36603dac98400
lixun2015 <lixun516@qq.com>
2020-01-11 23:08:16 +0800

    update language files

--- web/OJ/lang/cn.php
+++ web/OJ/lang/cn.php
@@ -26,41 +26,76 @@
   $MSG_MODIFY_USER = "修改帐号";
   $MSG_MAIL = "短消息";
   $MSG_ADMIN = "管理";
+  $MSG_USERINFO = "用户信息";
+  $MSG_MY_SUBMISSIONS="我的提交"; 
+  $MSG_MY_CONTESTS="我的$MSG_CONTEST"; 
+  $MSG_HIDETAG = "隐藏标签";
+  $MSG_SHOWTAG = "显示标签";
 
   // 比赛导航栏
   $MSG_BACK_TO_HOME = "返回主页";
-  $MSG_PROBLEM = "问题";
+  $MSG_PROBLEM = "题目";
   $MSG_STATISTICS = "统计";
-  $BACK_TO_CONTEST = "返回比赛";
-
+  $BACK_TO_CONTEST = "返回";
+  
   // problemset.php
   $MSG_SEARCH = "查找";
   $MSG_PROBLEM_ID = "题号";
   $MSG_TITLE = "标题";
-  $MSG_SOURCE = "来源";
   $MSG_SUBMIT = "提交";
-  $MSG_SCORE = "分数";
+  $MSG_SCORE = "分值";
+  $MSG_KEYWORDS = "关键词";
+  $MSG_TAGS = "标签";
+  $MSG_AUTHOR = "提交人";
 
   //ranklist.php
-  $MSG_Number = "名次";
-  $MSG_NICK = "昵称";
-  $MSG_SOVLED = "解决";
+  $MSG_RANK = "名次";
+  $MSG_NICK = "姓名";
+  $MSG_SOLVED = "解决";
   $MSG_RATIO = "比率";
   $MSG_LEVEL = "等级";
   $MSG_STRENGTH = "实力";
+  $MSG_RANKTIPS = "此排名非实时数据排名，请访问自己的用户信息页更新数据。";
+  $MSG_Wrong = "错误";
 
   // contestRank.php
-  $MSG_RANK = "排名";
   $MSG_REAL_NAME = "真实姓名";
   $MSG_USER = "用户";
   $MSG_PENALTY = "罚时";
   $MSG_DOWNLOAD_RANK = "下载排名";
-
-
+  
+  //其他
+  $MSG_SUBMISSIONS = "提交量";
+  $MSG_SUBMISSION = "提交";
+  $MSG_SUNMITTOTAL = "总提交";
+  $MSG_HINTS= "访问量";  
+  $MSG_SERVERTIME = "服务器时间";
+  $MSG_MAINTAINER = "Maintainer List";
+  $MSG_ALL = "全部";
+  $MSG_GO = "跳转";
+  $MSG_EDIT = "编辑";
+  $MSG_DEL = "删除";
+  $MSG_MY = "我的";
+  $MSG_NO = "没有";
+  $MSG_LoginError = "用户名或密码或".$MSG_CONTEST."ID错误，或账号未激活!";  
+  $MSG_Tried = "正在努力的".$MSG_PROBLEM;
+  $MSG_Recommended = "推荐".$MSG_PROBLEM;
+  $MSG_OldPasswd = "旧密码";
+  $MSG_NewPasswd = "新密码";
+  $MSG_TOTAL = "合计";
+  $MSG_Available = "启用";
+  $MSG_Reserved = "未启用";
+  $MSG_CreatedDate = "创建日期";
+  $MSG_Solution = "单个提交代码";
+  $MSG_Importance = "优先级";
+  $MSG_Content = "内容";
+  $MSG_OpenSource ="开放源码";
+  $MSG_RankingExcludedUsers = "排名不包括以下用户";
+    
   /*
   下面的代码请不要乱动，以免出问题！！！
   */
-  
+  //status.php
   $MSG_Pending="等待";
   $MSG_Pending_Rejudging="等待重判";
   $MSG_Compiling="编译中";
@@ -72,30 +107,28 @@
   $MSG_Memory_Limit_Exceed="内存超限";
   $MSG_Output_Limit_Exceed="输出超限";
   $MSG_Runtime_Error="运行错误";
-  $MSG_Compile_Error="编译错误";
+  $MSG_TEST_RUN="运行完成";
+  $MSG_Compile_Error="编译错误";  
   $MSG_Runtime_Click="运行错误(点击看详细)";
   $MSG_Compile_Click="编译错误(点击看详细)";
   $MSG_Compile_OK="编译成功";
   $MSG_Click_Detail="点击看详细";
-   $MSG_Manual="人工判题";
-   $MSG_OK="确定";
+  $MSG_RESULT="结果";
+  $MSG_MEMORY="内存";
+  $MSG_TIME="耗时";
+  $MSG_LANG="语言";
+  $MSG_CODE_LENGTH="代码长度";
+  $MSG_SUBMIT_TIME="提交时间";
+  $MSG_Manual="人工判题";
+  $MSG_OK="确定";
   $MSG_Explain="输入判定原因与提示";
+  $MSG_RUNID="提交编号";
+  $MSG_FILTER = "过滤";
+  $MSG_RESET = "重置";
   
-  //fool's day
-if(date('m')==4&&date('d')==1&&rand(0,100)<10){
-        $MSG_Accepted="人品问题-愚人节快乐";
-$MSG_Presentation_Error="人品问题-愚人节快乐";
-$MSG_Wrong_Answer="人品问题-愚人节快乐";
-$MSG_Time_Limit_Exceed="人品问题-愚人节快乐";
-$MSG_Memory_Limit_Exceed="人品问题-愚人节快乐";
-$MSG_Output_Limit_Exceed="人品问题-愚人节快乐";
-$MSG_Runtime_Error="人品问题-愚人节快乐";
-$MSG_Compile_Error="人品问题-愚人节快乐";
-$MSG_Compile_OK="人品问题-愚人节快乐";
-}
-    $MSG_TEST_RUN="运行完成";
+  //problemstatistics.php      
   $MSG_TR="测试运行";
-
+  
   $MSG_PD="等待";
   $MSG_PR="等待重判";
   $MSG_CI="编译中";
@@ -109,24 +142,14 @@ $MSG_Compile_OK="人品问题-愚人节快乐";
   $MSG_RE="运行错误";
   $MSG_CE="编译错误";
   $MSG_CO="编译成功";
-  
-  $MSG_RUNID="运行编号";
-  
-  $MSG_PROBLEM="问题";
-  $MSG_RESULT="结果";
-  $MSG_MEMORY="内存";
-  $MSG_TIME="耗时";
-  $MSG_LANG="语言";
-  $MSG_CODE_LENGTH="代码长度";
-  $MSG_SUBMIT_TIME="提交时间";
-  
-  
+
+
   //registerpage.php
-  $MSG_USER_ID="用户名（学号）";
+  $MSG_USER_ID="用户名(学号)";
   $MSG_PASSWORD="密码";
   $MSG_REPEAT_PASSWORD="重复密码";
   $MSG_SCHOOL="学校";
-  $MSG_EMAIL="电子邮件";
+  $MSG_EMAIL="电子邮箱";
   $MSG_REG_INFO="注册信息";
   $MSG_VCODE="验证码";
 
@@ -135,38 +158,147 @@ $MSG_Compile_OK="人品问题-愚人节快乐";
   $MSG_Description="题目描述"  ;
   $MSG_Input="输入"  ;
   $MSG_Output= "输出" ;
+  $MSG_Samples = "样例";
   $MSG_Sample_Input= "样例输入" ;
   $MSG_Sample_Output= "样例输出" ;
   $MSG_HINT= "提示" ;
   $MSG_Source= "来源" ;
+  $MSG_Limit = "限制";
+  $MSG_Times = "时间";
   $MSG_Time_Limit="时间限制";
   $MSG_Memory_Limit="内存限制";
+  $MSG_TestData = "测试数据";
+  $MSG_Codes = "查看代码";
+  $MSG_SolutionVideo = "帮助视频";
+  $MSG_WatchVideo = "点击观看帮助视频";
+  $MSG_ToggleSamples = "隐藏/显示";
+  $MSG_ShowAfterTrying = "几次之后显示本样例";
+  $MSG_AddSapmple = "增加一组样例";
+  $MSG_RemoveSample = "删除最后一组样例";
+  $MSG_SampleDataIsUpdated = "测试样例文件已更新！";
+  $MSG_AddMoreTestData = "添加更多的样例数据";
+  $MSG_EditOK = "编辑完毕！";
+  $MSG_SeeProblem = "查看题目！";
+	
   //admin menu
-  $MSG_SEEOJ="查看前台";
+  $MSG_DASHBOARD = "控制面板";
+  $MSG_SEEOJ="返回前台";
   $MSG_ADD="添加";
   $MSG_LIST="列表";
-  $MSG_NEWS="新闻";
+  $MSG_NEWS="公告";
   $MSG_TEAMGENERATOR="比赛队帐号生成器";
+  $MSG_TEAM = "比赛队";
   $MSG_SETMESSAGE="设置公告";
   $MSG_SETPASSWORD="修改密码";
-  $MSG_REJUDGE="重判题目";
+  $MSG_SET_USER_ID = "修改".$MSG_USER_ID;
+  $MSG_REJUDGE="重判";
   $MSG_PRIVILEGE="权限";
+  $MSG_Distribution = "权限分配";
   $MSG_GIVESOURCE="转移源码";
   $MSG_IMPORT="导入";
   $MSG_EXPORT="导出";
   $MSG_UPDATE_DATABASE="更新数据库";
   $MSG_ONLINE="在线";
+  $MSG_Copy = "复制";
+  $MSG_Logs = "日志";
+  $MSG_Operations = "操作";
+  $MSG_Old = "老";
+  $MSG_New = "新";
+  
   //contest
   $MSG_PRIVATE_WARNING="私有比赛或考试作业，您无权查看题目。";
   $MSG_WATCH_RANK="点击这里查看做题排名。";
   $MSG_Public="公开";
   $MSG_Private="私有";
+  $MSG_Practice = "练习赛";
+  $MSG_Special = "队赛专用(Special)";
   $MSG_Running="运行中";
   $MSG_Start="开始于";
+  $MSG_StartTime="开始时间";
+  $MSG_EndTime="结束时间";
   $MSG_TotalTime="总赛时";
   $MSG_LeftTime="剩余";
   $MSG_Ended="已结束";
   $MSG_Login="请登录后继续操作";
   $MSG_JUDGER="判题机";
-
+  $MSG_Announcement = "公告";
+  $MSG_Type = "类型";
+  $MSG_Name = "名称";
+  $MSG_ID = "编号";
+  $MSG_NearlyEnd = "即将结束，请注意掌握时间！";
+  $MSG_notStart = "尚未开始";
+  $MSG_TimeElapsed = "已耗时";
+  $MSG_TimeRemaining = "剩余时间";
+  $MSG_ContestIsClosed = "竞赛&作业已关闭！";
+  $MSG_Creator = "组织者";
+  
+  $MSG_HELP_ADD_NEWS="添加首页显示的".$MSG_NEWS;
+  $MSG_HELP_NEWS_LIST="管理已经发布的".$MSG_NEWS;
+  //$MSG_HELP_USER_LIST="对注册用户停用、启用帐号及编辑相关信息。";
+  $MSG_HELP_ADD_PROBLEM="手动添加新的题目，多组测试数据在添加后从题目列表'".$MSG_TestData."'按钮进入上传，新建题目<b>默认隐藏</b>，需在问题列表中点击'".$MSG_Reserved."'按钮切换启用状态。";
+  $MSG_HELP_PROBLEM_LIST="管理已有的题目和数据，上传数据可以用zip压缩不含目录的数据。";
+  $MSG_HELP_CONTEST_LIST="已有的比赛列表，修改时间和公开/私有等类型，尽量不要在开赛后调整题目列表。";
+  //$MSG_HELP_TEAMGENERATOR="批量生成大量比赛帐号、密码。小系统不要随便使用，可能产生垃圾帐号，无法删除。";
+  //$MSG_HELP_SETPASSWORD="重设指定用户的密码，对于管理员帐号需要先降级为普通用户才能修改。";
+  $MSG_HELP_REJUDGE="重判指定的题目、提交或比赛。";
+  $MSG_HELP_ADD_PRIVILEGE="给指定用户增加权限，包括管理员、教师、助教等权限。";
+  $MSG_HELP_PRIVILEGE_LIST="查看已有的特殊权限列表、进行删除操作。";
+  //$MSG_HELP_GIVESOURCE="将导入系统的标程赠与指定帐号，用于训练后辅助未通过的人学习参考。";
+  //$MSG_HELP_EXPORT_PROBLEM="将系统中的题目以fps.xml文件的形式导出。";
+  //$MSG_HELP_IMPORT_PROBLEM="导入从官方群共享或tk.hustoj.com下载到的fps.xml文件。";
+  
+  $MSG_HELP_AC="答案正确，请再接再厉。"; 
+  $MSG_HELP_PE="答案基本正确，但是格式不对。"; 
+  $MSG_HELP_WA="答案不对，仅仅通过样例数据的测试并不一定是正确答案，一定还有你没想到的地方，点击查看系统可能提供的对比信息。"; 
+  $MSG_HELP_TLE="运行超出时间限制，检查下是否有死循环，或者应该有更快的计算方法"; 
+  $MSG_HELP_MLE="超出内存限制，数据可能需要压缩，检查内存是否有泄露"; 
+  $MSG_HELP_OLE="输出超过限制，你的输出比正确答案长了两倍，一定是哪里弄错了"; 
+  $MSG_HELP_RE="运行时错误，非法的内存访问，数组越界，指针漂移，调用禁用的系统函数。请点击后获得详细输出";
+  $MSG_HELP_CE="编译错误，请点击后获得编译器的详细输出"; 
+  
+  $MSG_judge_result=Array(
+    $MSG_Pending,              //0
+    $MSG_Pending_Rejudging,    //1
+    $MSG_Compiling,            //2
+    $MSG_Running_Judging,      //3
+    $MSG_HELP_AC,              //4
+    $MSG_HELP_PE,              //5
+    $MSG_HELP_WA,              //6
+    $MSG_HELP_TLE,             //7
+    $MSG_HELP_MLE,             //8
+    $MSG_HELP_OLE,             //9
+    $MSG_HELP_RE,              //10
+    $MSG_HELP_CE,              //11
+    $MSG_Compile_OK,           //12
+    $MSG_TEST_RUN              //13
+  );
+  
+  //$MSG_HELP_MORE_TESTDATA_LATER="更多组测试数据，请在题目添加完成后补充"; 
+  //$MSG_HELP_SPJ="特殊裁判的使用，请参考<a href='https://cn.bing.com/search?q=hustoj+special+judge' target='_blank'>搜索hustoj special judge</a>"; 
+  
+  //$MSG_WARNING_LOGIN_FROM_DIFF_IP="从不同的ip地址登录";
+  //$MSG_LOSTPASSWORD_MAILBOX="请到您邮箱的垃圾邮件文件夹寻找验证码，并填写到这里";
+  //$MSG_LOSTPASSWORD_WILLBENEW="如果填写正确，通过下一步验证，这个验证码就自动成为您的新密码！";
+  
+  //privilege_distribution.php
+  $MSG_enter_admin_page = "访问管理页面";
+  $MSG_edit_default_problem = $MSG_EDIT.$MSG_PROBLEM;
+  $MSG_rejudge = $MSG_REJUDGE;
+  $MSG_edit_news = $MSG_EDIT.$MSG_NEWS;
+  $MSG_edit_contest = $MSG_EDIT.$MSG_CONTEST;
+  $MSG_download_ranklist = $MSG_DOWNLOAD_RANK;
+  $MSG_generate_team ="generate_team";
+  $MSG_edit_user_profile = $MSG_EDIT.$MSG_USER;
+  $MSG_edit_privilege_group =$MSG_EDIT.$MSG_PRIVILEGE."组";
+  $MSG_edit_privilege_distribution=$MSG_EDIT.$MSG_Distribution;
+  $MSG_inner_function ="inner_function";
+  $MSG_see_hidden_default_problem ="查看隐藏的题目";
+  $MSG_see_hidden_user_info ="查看未激活用户的信息";
+  $MSG_see_wa_info_out_of_contest ="查看比赛之外正提交代码的错误信息";
+  $MSG_see_wa_info_in_contest ="查看比赛中提交代码的错误信息";
+  $MSG_see_source_out_of_contest ="查看比赛之外正常提交的源代码";
+  $MSG_see_source_in_contest ="查看比赛中提交的源代码";
+  $MSG_see_compare ="see_compare";
+  $MSG_upload_files ="上传文件";
+  $MSG_watch_solution_video ="观看".$MSG_SolutionVideo;
 ?>
--- web/OJ/lang/en.php
+++ web/OJ/lang/en.php
@@ -26,41 +26,75 @@
   $MSG_MODIFY_USER = "ModifyUser";
   $MSG_MAIL = "Mail";
   $MSG_ADMIN = "Admin";
+  $MSG_USERINFO = "User Info";
+  $MSG_MY_SUBMISSIONS = "My Status";
+  $MSG_MY_CONTESTS="My $MSG_CONTEST"; 
+  $MSG_HIDETAG = "Hide Tag";
+  $MSG_SHOWTAG = "Show Tag";
   
   // 比赛导航栏
   $MSG_BACK_TO_HOME = "BackToHome";
-  $MSG_PROBLEM = "Problem";
-  $MSG_STATISTICS = "Statistics"; 
+  $MSG_PROBLEM = " Problem ";
+  $MSG_STATISTICS = " Statistics"; 
   $BACK_TO_CONTEST = "BackToContest";
-
+  
   // problemset.php
   $MSG_SEARCH = "Search";
   $MSG_PROBLEM_ID = "Prob ID";
   $MSG_TITLE = "Title";
-  $MSG_SOURCE = "Source";
   $MSG_SUBMIT = "Submit";
   $MSG_SCORES = "Scores";
+  $MSG_KEYWORDS = "Keywords";
+  $MSG_TAGS = "Tag";
+  $MSG_AUTHOR = "Author";
 
   // ranklist.php
-  $MSG_Number = "No.";
+  $MSG_RANK = "Rank";
   $MSG_NICK = "Nick Name";
   $MSG_SOLVED = "Solved";
   $MSG_RATIO = "Ratio";
   $MSG_LEVEL = "Level";
   $MSG_STRENGTH = "Strength";
-
+  $MSG_RANKTIPS = "Standings won't update automatically, Please visit your user info page to update your information.";
+  $MSG_Wrong = " Wrong";
+  
   // contestRank.php
-  $MSG_RANK = "Rank";
   $MSG_USER = "User";
   $MSG_REAL_NAME = "Real name";
   $MSG_PENALTY = "Penalty";
   $MSG_DOWNLOAD_RANK = "Download Rank";
-
-
+      
+   //其他
+  $MSG_SUBMISSIONS = "Submissions";
+  $MSG_SUBMISSION = "Submission";
+  $MSG_SUNMITTOTAL = "Total";
+  $MSG_HINTS= "Hits";
+  $MSG_SERVERTIME = "Server Time";
+  $MSG_MAINTAINER = "Maintainer List";
+  $MSG_ALL = "All";
+  $MSG_GO = "Go";
+  $MSG_EDIT = "Edit ";
+  $MSG_DEL = "Delete";
+  $MSG_MY = "My ";
+  $MSG_NO = "No ";
+  $MSG_LoginError = "UserID or Password or ContestID Wrong, or UesrID is not actived!";
+  $MSG_Tried = "Tried";
+  $MSG_Recommended = "Recommended";
+  $MSG_OldPasswd = "Old Password";
+  $MSG_NewPasswd = "New Password";
+  $MSG_TOTAL = "Total";
+  $MSG_Available = "Available";
+  $MSG_Reserved = "Reserved";
+  $MSG_CreatedDate = "Created Date";
+  $MSG_Solution = "Solution";
+  $MSG_Importance = "Importance";
+  $MSG_Content = "Content";
+  $MSG_OpenSource ="Open source";
+  $MSG_RankingExcludedUsers = "Ranking Excluded Users";
+	
   /*
   下面的代码请不要乱动，以免出问题！！！
   */
-
   //status.php
   $MSG_Pending="Pending";
   $MSG_Pending_Rejudging="Pending Rejudging";
@@ -73,16 +107,13 @@
   $MSG_Memory_Limit_Exceed="Memory Limit Exceed";
   $MSG_Output_Limit_Exceed="Output Limit Exceed";
   $MSG_Runtime_Error="Runtime Error";
-$MSG_TEST_RUN="Test Running Done";
-  $MSG_Compile_Error="Compile Error";
-   
+  $MSG_TEST_RUN="Test Running Done";
+  $MSG_Compile_Error="Compile Error";   
   $MSG_Runtime_Click="Runtime Error(Click)";
   $MSG_Compile_Click="Compile Error(Click)";
   $MSG_Click_Detail="Click To View Detail";
   $MSG_Compile_OK="Compile OK";
-  $MSG_RUNID="RunID";
-  $MSG_USER="User";
-  $MSG_PROBLEM="Problem";
+
   $MSG_RESULT="Result";
   $MSG_MEMORY="Memory";
   $MSG_TIME="Time";
@@ -92,6 +123,10 @@ $MSG_TEST_RUN="Test Running Done";
   $MSG_Manual="Manual Judge";
   $MSG_OK="OK";
   $MSG_Explain="Type reason or explaination";
+  $MSG_RUNID="Run ID";
+  $MSG_FILTER = "Filter";
+  $MSG_RESET = "Reset";
+  
   //problemstatistics.php
   $MSG_PD="PD";
   $MSG_PR="PR";
@@ -120,45 +155,132 @@ $MSG_TEST_RUN="Test Running Done";
   //problem.php
   $MSG_NO_SUCH_PROBLEM="Problem is not Available!";
   $MSG_Description="Description"  ;
-  $MSG_Input="Input"  ;
-  $MSG_Output= "Output" ;
+  $MSG_Input="Input "  ;
+  $MSG_Output= "Output " ;
+  $MSG_Samples = "Samples";
   $MSG_Sample_Input= "Sample Input" ;
   $MSG_Sample_Output= "Sample Output" ;
   $MSG_HINT= "HINT" ;
   $MSG_Source= "Source" ;
+  $MSG_Limit = "Limit";
+  $MSG_Times = "Time";
   $MSG_Time_Limit="Time Limit";
   $MSG_Memory_Limit="Memory Limit";
+  $MSG_TestData = "Test Data";
+  $MSG_Codes = "Codes";
+  $MSG_SolutionVideo = "Solution Video";
+  $MSG_WatchVideo = "Click To Watch The Video.";
+  $MSG_ToggleSamples = "Toggle samples";
+  $MSG_ShowAfterTrying = "Show after some trying";
+  $MSG_AddSapmple = "Add a sapmple";
+  $MSG_RemoveSample = "Remove the last sample";
+  $MSG_SampleDataIsUpdated = "Sample data file Updated!";
+  $MSG_AddMoreTestData = "Add More Test Data";
+  $MSG_EditOK = "Edit OK!";
+  $MSG_SeeProblem = "See The Problem!";
   
   //admin menu
+  $MSG_DASHBOARD = "DASHBOARD";
   $MSG_SEEOJ="SeeOJ";
-  $MSG_ADD="Add";
-  $MSG_LIST="List";
+  $MSG_ADD="Add ";
+  $MSG_LIST=" List";
   $MSG_NEWS="News";
-  $MSG_TEAMGENERATOR="TeamGenerator";
-  $MSG_SETMESSAGE="SetMessage";
-  $MSG_SETPASSWORD="ChangePassWD";
-  $MSG_REJUDGE="Rejudge";
+  $MSG_TEAMGENERATOR="Team Generator";
+  $MSG_TEAM = "Team ";
+  $MSG_SETMESSAGE="Set Message";
+  $MSG_SETPASSWORD="Change Password";
+  $MSG_SET_USER_ID = "Change User ID";
+  $MSG_REJUDGE="Rejudge ";
   $MSG_PRIVILEGE="Privilege";
-  $MSG_GIVESOURCE="GiveSource";
+  $MSG_Distribution = "Privilege Distribution";
+  $MSG_GIVESOURCE="Give Source";
   $MSG_IMPORT="Import";
   $MSG_EXPORT="Export";
-  $MSG_UPDATE_DATABASE="UpdateDatabase";
-  $MSG_ONLINE="Online";
+  $MSG_UPDATE_DATABASE="Update Database";
+  $MSG_ONLINE="Online";  
+  $MSG_Copy = "Copy";
+  $MSG_Logs = "Logs";
+  $MSG_Operations = "Operations";
+  $MSG_Old = "Old ";
+  $MSG_New = "New ";
    
   //contest.php
   $MSG_PRIVATE_WARNING="This is a private contest which you don't have privilege.";
   $MSG_WATCH_RANK="Click here to watch contest rank.";
-
   $MSG_Public="Public";
   $MSG_Private="Private";
+  $MSG_Practice = "Practice";
+  $MSG_Special = "Team only(Special)";
   $MSG_Running="Running";
   $MSG_Start="Start";
+  $MSG_StartTime="Start Time";
+  $MSG_EndTime="End Time";
   $MSG_TotalTime="Total";
   $MSG_LeftTime="Left";
-
-  $MSG_Ended="Finished";
-  
+  $MSG_Ended="Finished";  
   $MSG_Login="Please Login";
   $MSG_JUDGER="Judger";
+  $MSG_Announcement = "Announcement";
+  $MSG_Type = "Type";
+  $MSG_Name = "Name";
+  $MSG_ID = "ID";
+  $MSG_NearlyEnd = "Contest is nearly the end!";
+  $MSG_notStart = "Contest hasn't started now!";
+  $MSG_TimeElapsed = "Time elapsed";
+  $MSG_TimeRemaining = "Time remaining";
+  $MSG_ContestIsClosed = "Contest is closed!";
+  $MSG_Creator = "Creator";
+  
+  $MSG_HELP_ADD_NEWS="add news for the homepage";
+  $MSG_HELP_NEWS_LIST="edit or shutdown published news";
+  $MSG_HELP_USER_LIST="enable/disable user";
+  $MSG_HELP_ADD_PROBLEM="add new problem,multi test cases can be added after problem added ,using TestData button on the Problem List";
+  $MSG_HELP_PROBLEM_LIST="manage existing problems, test case files can be uploaded within a zip file and decompress later";
+  $MSG_HELP_CONTEST_LIST="The existing list of contests, modification time and public / private……, try not to adjust the list of items after the start.";
+  $MSG_HELP_TEAMGENERATOR="Generate accounts and passwords in batches for competitors from different schools. Small system should not be used casually, may produce garbage accounts, which can not be easily deleted.";
+  $MSG_HELP_SETPASSWORD="Reset the password for the specified user, for administrator account need to downgrade to ordinary users to modify.";
+  $MSG_HELP_REJUDGE="To review a specified subject, submission, or contest.";
+  $MSG_HELP_ADD_PRIVILEGE=" adds permissions to designated users, including administrators, teacher, teacher_assistant, code viewer and other permissions. ";
+  $MSG_HELP_PRIVILEGE_LIST= "looks at the existing list of special permissions and deletes them. ";
+  $MSG_HELP_GIVESOURCE=" transfers the label of the import system to the designated account number, which is used to assist the person who has not passed the training after the training. ";
+  $MSG_HELP_EXPORT_PROBLEM= "export the problems of the system in the form of fps.xml file. ";
+  $MSG_HELP_IMPORT_PROBLEM=" import fps.xml files downloaded from internet or tk.hustoj.com. ";
 
+  $MSG_HELP_PE="Your output format is not exactly the same as the judge's output, although your answer to the problem is correct. Check your output for spaces, blank lines,etc against the problem output specification"; 
+  $MSG_HELP_WA=" Correct solution not reached for the inputs. The inputs and outputs that we use to test the programs are not public (it is recomendable to get accustomed to a true contest dynamic ;-)"; 
+  $MSG_HELP_TLE="Your program tried to run during too much time"; 
+  $MSG_HELP_MLE="Your program tried to use more memory than the judge default settings"; 
+  $MSG_HELP_OLE="Your program tried to write too much information. This usually occurs if it goes into a infinite loop. Currently the output limit is 1M bytes"; 
+  $MSG_HELP_RE="All the other Error on the running phrase will get Runtime Error, such as 'segmentation fault','floating point exception','used forbidden functions', 'tried to access forbidden memories' and so on";
+  $MSG_HELP_CE="The compiler (gcc/g++/gpc) could not compile your ANSI program. Of course, warning messages are not error messages. Click the link at the judge reply to see the actual error message"; 
+  $MSG_HELP_AC="Congratulations!"; 
+  
+  $MSG_HELP_MORE_TESTDATA_LATER="more testdata can be added later after this problem added."; 
+  $MSG_HELP_SPJ="<a href='https://cn.bing.com/search?q=hustoj+special+judge' target='_blank'>search hustoj special judge</a>for more details"; 
+ 
+  $MSG_WARNING_LOGIN_FROM_DIFF_IP="Login from a diffrent ip ";
+  $MSG_LOSTPASSWORD_MAILBOX="Input the Code sended to your email (Trash Box )";
+  $MSG_LOSTPASSWORD_WILLBENEW="if it's correct, it will be the new password";
+
+  //privilege_distribution.php
+  $MSG_enter_admin_page ="enter_admin_page";
+  $MSG_edit_default_problem ="edit_default_problem";
+  $MSG_rejudge ="rejudge";
+  $MSG_edit_news ="edit_news";
+  $MSG_edit_contest ="edit_contest";
+  $MSG_download_ranklist ="download_ranklist";
+  $MSG_generate_team ="generate_team";
+  $MSG_edit_user_profile ="edit_user_profile";
+  $MSG_edit_privilege_group ="edit_privilege_group";
+  $MSG_edit_privilege_distribution="edit_privilege_distribution";
+  $MSG_inner_function ="inner_function";
+  $MSG_see_hidden_default_problem ="see_hidden_default_problem";
+  $MSG_see_hidden_user_info ="see_hidden_user_info";
+  $MSG_see_wa_info_out_of_contest ="see_wa_info_out_of_contest";
+  $MSG_see_wa_info_in_contest ="see_wa_info_in_contest";
+  $MSG_see_source_out_of_contest ="see_source_out_of_contest";
+  $MSG_see_source_in_contest ="see_source_in_contest";
+  $MSG_see_compare ="see_compare";
+  $MSG_upload_files ="upload_files";
+  $MSG_watch_solution_video ="watch_solution_video";
 ?>
