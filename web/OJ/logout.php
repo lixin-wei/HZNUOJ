116d4135c1054d3b389bf63b7cff530198f435d9
lixun2015 <lixun516@qq.com>
2020-01-12 22:30:12 +0800

    修改：注销后跳转到首页

--- web/OJ/logout.php
+++ web/OJ/logout.php
@@ -1,8 +1,5 @@
 <?php session_start();
 unset($_SESSION['user_id']);
 session_destroy();
-
-echo "<script language='javascript'>\n";
-echo "history.go(-1);\n";
-echo "</script>";
+header("Location:index.php");
 ?>
