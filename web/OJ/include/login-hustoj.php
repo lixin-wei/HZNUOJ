0d6957ef1200618f76d593d0b8fe1de8ed88cefa
lixun2015 <lixun516@qq.com>
2020-01-12 21:19:07 +0800

    fixed:没激活账户也能登录,登录时间不更新

--- web/OJ/include/login-hustoj.php
+++ web/OJ/include/login-hustoj.php
@@ -14,12 +14,18 @@
     global $LOGIN_DEFUNCT;
     global $mysqli;
     $user_id=$mysqli->real_escape_string($user_id);
+	  $checkResult = false ;
     $pass2 = 'No Saved';
-  session_destroy();
+    session_destroy();
     session_start();
-
-    if (!is_numeric($cid)) {
-      $canLogin = true;
+    $ip = ($_SERVER['REMOTE_ADDR']);
+	if( !empty( $_SERVER['HTTP_X_FORWARDED_FOR'] ) ){
+	    $REMOTE_ADDR = $_SERVER['HTTP_X_FORWARDED_FOR'];
+	    $tmp_ip=explode(',',$REMOTE_ADDR);
+	    $ip =(htmlentities($tmp_ip[0],ENT_QUOTES,"UTF-8"));
+	}
+   if (!is_numeric($cid)) {
+	   $canLogin = true;
       if ($LOGIN_DEFUNCT) {
         $sql = "SELECT * FROM privilege WHERE user_id='$user_id' AND defunct='N' AND rightstr!='teacher_assistant'";
         $result = $mysqli->query($sql);
@@ -27,19 +33,22 @@
         $result->free();
       }
       if ($canLogin) {
-        $sql="INSERT INTO `loginlog` (user_id,password,ip,`time`)VALUES('$user_id','$pass2','".$_SERVER['REMOTE_ADDR']."',NOW())";
-        @$mysqli->query($sql) or die($mysqli->error);
-        $sql="SELECT `user_id`,`password` FROM `users` WHERE `user_id`='".$user_id."'";
-        $result=$mysqli->query($sql);
+        $sql="SELECT `user_id`,`password` FROM `users` WHERE `user_id`='$user_id' and defunct='N' ";
+	    $result = $mysqli->query($sql);
         $row = $result->fetch_array();
         if($row && pwCheck($password,$row['password'])){
           $user_id=$row['user_id'];
-          $result->free();
-          return $user_id;
-        }
-        $result->free();
-      } else {
-        return -1;
+		  $sql="update users set accesstime=now() where user_id='$user_id'";
+      $mysqli->query($sql) or die($mysqli->error);
+		  $checkResult = $user_id;
+		  $sql="INSERT INTO `loginlog` (user_id,password,ip,`time`)VALUES('$user_id','login OK','$ip',NOW())";
+        } else {
+		  $sql="INSERT INTO `loginlog` (user_id,password,ip,`time`)VALUES('$user_id','login Failed','$ip',NOW())";
+		}
+		
+      }else {
+		  $sql="INSERT INTO `loginlog` (user_id,password,ip,`time`)VALUES('$user_id','login Failed','$ip',NOW())";
+          $checkResult = -1;
       }
     } else {
       $sql="SELECT `user_id`,`password`,contest_id FROM `team` WHERE `user_id`='$user_id' AND contest_id='$cid'";
@@ -47,15 +56,17 @@
       $row = $result->fetch_array();
       if($row && pwCheck($password,$row['password'])){
         $user_id=$row['user_id'];
-        $_SESSION['contest_id'] = $row['contest_id'];
-        $pass = $row['password'];
-        $result->free();
-        $sql="INSERT INTO `loginlog`(user_id,password,ip,`time`) VALUES('$user_id','$pass','".$_SERVER['REMOTE_ADDR']."',NOW())";
-        @$mysqli->query($sql) or die($mysqli->error);
-        return $user_id;
-      }
-      $result->free();
+        $_SESSION['contest_id'] = $row['contest_id'];        
+		$sql="update team set accesstime=now() where user_id='$user_id' AND contest_id='$cid'";
+	    $mysqli->query($sql) or die($mysqli->error);
+		$checkResult = $user_id;
+		$sql="INSERT INTO `loginlog` (user_id,password,ip,`time`)VALUES('$user_id','login OK','$ip',NOW())";
+      } else {
+		$sql="INSERT INTO `loginlog` (user_id,password,ip,`time`)VALUES('$user_id','login Failed','$ip',NOW())";
+	  }
     }
-    return false; 
+      $result->free();
+    @$mysqli->query($sql) or die($mysqli->error); //插入登录日志
+    return $checkResult; 
   }
 ?>
