14d6ce00bfa41c773b736ff47bc3880d4fc45296
lixun2015 <lixun516@qq.com>
2020-01-12 10:29:06 +0800

    根据是否是比赛页($_GET['cid'])加载不同的header文件

--- web/OJ/template/hznu/error.php
+++ web/OJ/template/hznu/error.php
@@ -1,9 +1,12 @@
 <?php
 	$title="Error";
-	require_once("header.php");
+    if ($_GET['cid']) require_once("contest_header.php");
+    else require_once("header.php");
 	if ($view_errors=="") { //default text
 		$view_errors="You don't have the privilege to view this page!";
 	}
 ?>
+<div class="am-container">
 <h1 align="center"><?php echo $view_errors ?></h1>
+</div>
 <?php require_once("footer.php") ?>
