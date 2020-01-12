0ea28d3c2f561357fd90351d6e9e347be5038a17
lixun2015 <lixun516@qq.com>
2020-01-12 20:00:36 +0800

    恢复验证码的使用

--- web/OJ/template/hznu/registerpage.php
+++ web/OJ/template/hznu/registerpage.php
@@ -9,52 +9,52 @@
   **/
 ?>
 
-<?php $title="Register";?>
+<?php $title=$MSG_REGISTER;?>
 <?php
   include "header.php";
 ?>
 <div class="am-g">
   <div class="am-u-lg-6 am-u-md-8 am-u-sm-centered">
-    <h1 style="margin-top:40px; margin-bottom: 0px;">Register Page</h1>
+    <h1 style="margin-top:40px; margin-bottom: 0px;"><?php echo $MSG_REG_INFO ?></h1>
     <hr>
-    <form class="am-form am-form-horizontal" action="register.php" method="post" data-am-validator>
+    <form class="am-form am-form-horizontal" action="register.php" method="post"  data-am-validator >
     <?php include_once $_SERVER['DOCUMENT_ROOT']."/OJ/include/set_post_key.php"?>
     <div class="am-form-group">
       <label for="username" class="am-u-sm-4 am-form-label">
-        <font color='red'><b>*</b></font>&nbsp;User ID:
+        <font color='red'><b>*</b></font>&nbsp;<?php echo $MSG_USER_ID ?>:
       </label>
       <div class="am-u-sm-8">
-        <input type="text" name="user_id" id="username" value="" placeholder="3-20 characters, unique" style="width:300px;" minlength="3" maxlength="20" required/>
+        <input type="text" name="user_id" id="username" value="" placeholder="设定3-20位用户名" style="width:300px;" minlength="3" maxlength="20" required/>
       </div>
     </div>
     <div class="am-form-group">
       <label for="pwd" class="am-u-sm-4 am-form-label">
-        <font color='red'><b>*</b></font>&nbsp;Password:
+        <font color='red'><b>*</b></font>&nbsp;<?php echo $MSG_PASSWORD ?>:
       </label>
       <div class="am-u-sm-8">
-        <input type="password" name="password" id="pwd" value="" placeholder="6-22 characters" style="width:300px;"
+        <input type="password" name="password" id="pwd" value="" minlength="6" maxlength="22" placeholder="设定6-22位密码" style="width:300px;"
           pattern="^[\@A-Za-z0-9\!\#\$\%\^\&\*\.\~]{6,22}$" required/>
       </div>
     </div>
     <div class="am-form-group">
       <label for="rname" class="am-u-sm-4 am-form-label">
-        <font color='red'><b>*</b></font>&nbsp;Repeat Password:
+        <font color='red'><b>*</b></font>&nbsp;<?php echo $MSG_REPEAT_PASSWORD ?>:
       </label>
       <div class="am-u-sm-8">
-        <input type="password" id="rname" name="rptpassword"value="" style="width:300px;" placeholder="Repeat your password"
+        <input type="password" id="rname" name="rptpassword" value="" style="width:300px;" placeholder="确认密码"
           data-equal-to="#pwd" required/>
       </div>
     </div>
     <div class="am-form-group">
-      <label for="nc" class="am-u-sm-4 am-form-label">Nick Name: </label>
+      <label for="nc" class="am-u-sm-4 am-form-label"><font color='red'><b>*</b></font>&nbsp;<?php echo $MSG_NICK ?>: </label>
       <div class="am-u-sm-8">
-        <input type="text" id="nc" name="nick" value="" style="width:300px;" placeholder="100 characters at most"/>
+        <input name="nick" type="text" id="nc" placeholder="真实姓名" style="width:300px;" value="" maxlength="6"  pattern="^[\u4e00-\u9fa5]{2,6}$"/>
       </div>
     </div>
     <div class="am-form-group">
-      <label for="school" class="am-u-sm-4 am-form-label">School: </label>
+      <label for="school" class="am-u-sm-4 am-form-label"><font color='red'><b>*</b></font>&nbsp;<?php echo $MSG_SCHOOL ?>: </label>
       <div class="am-u-sm-8">
-        <input type="text" id="school" name="school"value="" style="width:300px;" placeholder="Your school" />
+        <input type="text" id="school" name="school" value="" style="width:300px;" placeholder="就读学校" pattern="^[\u4e00-\u9fa5]{3,20}$"/>
       </div>
     </div>
     <div class="am-form-group">
@@ -85,16 +85,25 @@
     </div>
     <div class="am-form-group">
       <label for="email" class="am-u-sm-4 am-form-label">
-        <font color='red'><b>*</b></font>&nbsp;Email:
+        &nbsp;<?php echo $MSG_EMAIL ?>:
       </label>
       <div class="am-u-sm-8">
-        <input type="email" id="email" value="" name="email" style="width:300px;" placeholder="Your email"
+        <input type="email" id="email" value="" name="email" style="width:300px;" placeholder="<?php echo $MSG_EMAIL ?>"
           required/>
       </div>
     </div>
+    <?php if($OJ_VCODE): ?>
+        <div class="am-form-group">
+      <label for="vcode" class="am-u-sm-4 am-form-label"><font color='red'><b>*</b></font>&nbsp;<?php echo $MSG_VCODE ?>: </label>
+		<div class="am-u-sm-1">
+		<input name="vcode" type='text' style="width:150px;" size=4 maxlength="4" autocomplete="off" required></input></div>
+        <div class="am-u-sm-5">
+        <img style='width:100px;height:35px'alt="click to change" src='vcode.php' onclick="this.src='vcode.php#'+Math.random()"></div>
+    </div>
+    <?php endif ?>
     <div class="am-from-group">
       <div class="am-cf am-u-sm-offset-4 am-u-sm-3 am-u-end">
-            <input type="submit" name="submit" value="Register" class="am-btn am-btn-primary am-btn-sm am-fl am-btn-block">
+            <input type="submit" name="submit" value="<?php echo $MSG_REGISTER ?>" class="am-btn am-btn-primary am-btn-sm am-fl am-btn-block">
           </div>
         </div>
     </div>
