cb48321372c29e558a68be9e6b96b68776a92ae6
lixun2015 <lixun516@qq.com>
2020-01-13 19:19:22 +0800

    fixed : 重判contest中RunID时不会跳转到相应contest的status页面

--- web/OJ/admin/rejudge.php
+++ web/OJ/admin/rejudge.php
@@ -14,7 +14,7 @@ if (!HAS_PRI("rejudge")) {
 		$sql="delete from `sim` WHERE `s_id` in (select solution_id from solution where `problem_id`=".$rjpid.")";
 		$mysqli->query($sql) or die($mysqli->error);
 		$url="../status.php?problem_id=".$rjpid;
-		echo "Rejudged Problem ".$rjpid;
+		echo $MSG_REJUDGE.$MSG_PROBLEM_ID.":".$rjpid;
 		echo "<script>location.href='$url';</script>";
 	}
 	else if (isset($_POST['rjsid'])){
@@ -23,41 +23,54 @@ if (!HAS_PRI("rejudge")) {
 		$mysqli->query($sql) or die($mysqli->error);
 		$sql="delete from `sim` WHERE `s_id`=".$rjsid;
 		$mysqli->query($sql) or die($mysqli->error);
-		$url="../status.php?top=".($rjsid+1);
-		echo "Rejudged Runid ".$rjsid;
+		$sql="select contest_id from `solution` WHERE `solution_id`=".$rjsid;
+		$data=$mysqli->query($sql);
+		$cid=intval($data->fetch_row()[0]);
+		if ($cid>0)
+			$url="../status.php?cid=".$cid."&top=".($rjsid+1);
+		else
+			$url="../status.php?top=".($rjsid+1);
+		echo $MSG_REJUDGE.$MSG_RUNID.":".$rjsid;
+		$data->free();
 		echo "<script>location.href='$url';</script>";
 	}else if (isset($_POST['rjcid'])){
 		$rjcid=intval($_POST['rjcid']);
 		$sql="UPDATE `solution` SET `result`=1 WHERE `contest_id`=".$rjcid;
 		$mysqli->query($sql) or die($mysqli->error);
 		$url="../status.php?cid=".($rjcid);
-		echo "Rejudged Contest id :".$rjcid;
+		echo $MSG_REJUDGE.$MSG_CONTEST.$MSG_ID.":".$rjcid;
 		echo "<script>location.href='$url';</script>";
 	}
 
 }
 ?>
-	<title>Rejudge</title>
-	<h1>Rejudge</h1><hr/>
+    <title><?php echo $html_title.$MSG_REJUDGE ?></title>
+	<h1><?php echo $MSG_REJUDGE ?></h1>
+    <h5>&lt;<?php echo $MSG_HELP_REJUDGE ?>&gt;</h5>
+    <hr/>
 	<ol>
-	<li>Problem
+	<li><?php echo $MSG_PROBLEM ?>
 	<form class="form-inline" action='rejudge.php' method=post>
-		<input class="form-control" type=input name='rjpid'>	<input type='hidden' name='do' value='do'>
-		<button type=submit class="btn btn-default">Submit</button>
-		<?php require("../include/set_post_key.php");?>
+		<input class="form-control" type=input name='rjpid' placeholder="<?php echo $MSG_PROBLEM_ID ?>" >	<input type='hidden' name='do' value='do'>
+		<button type=submit class="btn btn-default"><?php echo $MSG_SUBMIT ?></button>
+		<?php require_once("../include/set_post_key.php");?>
 	</form>
-	<li>Solution
+    </li>
+	<li><?php echo $MSG_Solution ?>
 	<form class="form-inline" action='rejudge.php' method=post>
-		<input class="form-control" type=input name='rjsid'>	<input type='hidden' name='do' value='do'>
-		<button type=submit class="btn btn-default">Submit</button>
-    <?php require("../include/set_post_key.php");?>
+		<input class="form-control" type=input name='rjsid' placeholder="<?php echo $MSG_RUNID ?>">	<input type='hidden' name='do' value='do'>
+		<button type=submit class="btn btn-default"><?php echo $MSG_SUBMIT ?></button>
+    <?php require_once("../include/set_post_key.php");?>
 	</form>
-	<li>Contest
+    </li>
+	<li><?php echo $MSG_CONTEST  ?>
 	<form class="form-inline" action='rejudge.php' method=post>
-		<input class="form-control" type=input name='rjcid'>	<input type='hidden' name='do' value='do'>
-		<button type=submit class="btn btn-default">Submit</button>
-    <?php require("../include/set_post_key.php");?>
+		<input class="form-control" type=input name='rjcid' placeholder="<?php echo $MSG_CONTEST.$MSG_ID ?>" >	<input type='hidden' name='do' value='do'>
+		<button type=submit class="btn btn-default"><?php echo $MSG_SUBMIT ?></button>
+    <?php require_once("../include/set_post_key.php");?>
 	</form>
+    </li>
+    </ol>
 <?php 
   require_once("admin-footer.php")
 ?>
