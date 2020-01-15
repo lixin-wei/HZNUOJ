48d5756a9dbdec63c6fb4e845efdc1539a67c59d
lixun2015 <lixun516@qq.com>
2020-01-15 19:52:26 +0800

    增加题目状态批量改变功能

--- web/OJ/admin/problem_df_change.php
+++ web/OJ/admin/problem_df_change.php
@@ -10,13 +10,31 @@
   require_once("admin-header.php");
   require_once("../include/check_get_key.php");
   require_once("../include/my_func.inc.php");
-  if (!HAS_PRI("edit_".get_problemset($_GET['id'])."_problem")) {
-    require_once("error.php");
+  $plist = "";
+  sort($_POST['pid']);
+  foreach($_POST['pid'] as $i){
+  	if($plist) {
+        if(HAS_PRI("edit_".get_problemset($i)."_problem")) $plist.=','.intval($i);
+  	} else{
+    	if(HAS_PRI("edit_".get_problemset($i)."_problem")) $plist = $i;
+  	}
+  }  
+  if (isset($_GET['id'])&& !HAS_PRI("edit_".get_problemset($_GET['id'])."_problem")) {
+    require_once("./error.php");
     exit(1);
   }
-?>
-<?php $id=intval($_GET['id']);
+  if (!isset($_GET['id'])&& $plist=="") {
+    require_once("./error.php");
+    exit(1);
+  }
 
+//20190808 批量改变状态
+if(isset($_POST['enable'])&&$plist){
+  $sql = "UPDATE `problem` SET defunct='N' WHERE `problem_id` IN ($plist)";           
+}else if(isset($_POST['disable'])&&$plist){
+  $sql = "UPDATE `problem` SET defunct='Y' WHERE `problem_id` IN ($plist)";           
+}else{  
+  $id = intval($_GET['id']);
 $sql="SELECT `defunct` FROM `problem` WHERE `problem_id`=$id";
 $result=$mysqli->query($sql);
 $row=$result->fetch_row();
@@ -25,6 +43,7 @@ echo $defunct;
 $result->free();
 if ($defunct=='Y') $sql="update `problem` set `defunct`='N' where `problem_id`=$id";
 else $sql="update `problem` set `defunct`='Y' where `problem_id`=$id";
+}
 $mysqli->query($sql) or die($mysqli->error);
 ?>
 <script language=javascript>
