83a9a1c1f7a4c2e13461c98c0136ac35964687e9
lixun2015 <lixun516@qq.com>
2020-04-01 23:48:00 +0800

    增加题目是否启用的过滤，修改分页方式，另外通过关键词搜索的结果不分页

--- web/OJ/admin/problem_list.php
+++ web/OJ/admin/problem_list.php
@@ -15,112 +15,127 @@
     require_once("error.php");
     exit(1);
   }
-  // $keyword=$_GET['keyword'];
-  // $keyword=$mysqli->real_escape_string($keyword);
   if($_GET['page'])$page=$_GET['page'];
   else $page=1;
-  $page_cnt=100;
-  // get problems START
-  $res_set = $mysqli->query("SELECT set_name,set_name_show FROM problemset");
-  $problem_sets = array();
-  while($row = $res_set->fetch_array()) {
-    array_push($problem_sets,$row);
-  }
-  $set_name_show = array();
-  foreach ($problem_sets as $key => $val){
-    $set_name_show[$val["set_name"]]=$val['set_name_show'];
-  }
-  $first = true;
-  $sql = "";
-  $cnt = 0;
-  /* 获取sql语句中的筛选部分 start */
-if(isset($_GET['keyword'])&&trim($_GET['keyword'])!="") {
-    $keyword=$mysqli->real_escape_string($_GET['keyword']);
-    $filter_sql="(title like '%$keyword%' or source like '%$keyword%' or author like '%$keyword%' OR tag1 like '%$keyword%' OR tag2 like '%$keyword%' OR tag3 like '%$keyword%')";
-} else {
-    $filter_sql="1";
-}
-
-/* 获取sql语句中的筛选部分 end */
-  foreach ($problem_sets as $key => $val){
-    $set_name=$val["set_name"];
-    if($_GET['OJ']=='' || $_GET['OJ']==$set_name){
-      if(HAS_PRI("edit_".$set_name."_problem")){
-        $t_sql=" FROM `problem` WHERE $filter_sql AND problemset='$set_name'";
-      
-        //count the number of problem START
-        $res = $mysqli->query("SELECT COUNT('problem_id')".$t_sql);
-        $cnt += $res->fetch_array()[0];
-        //count the number of problem END
-
-        $t_sql = "SELECT `problem_id`, problemset ,`title`, `author`, source, `in_date`,`defunct`,`accepted`,`submit`,score, tag1, tag2, tag3 ".$t_sql;
-        if($first) $first = false;
-        else $t_sql = " UNION ".$t_sql;
-        $sql .= $t_sql;
+  if(isset($page)) $args['page']=$page;
+  function generate_url($data){
+      global $args;
+      $link="problem_list.php?";
+      foreach ($args as $key => $value) {
+          if(isset($data["$key"])){
+              $value=htmlentities($data["$key"]);
+              $link.="&$key=$value";
+          }
+          else if($value){
+              $link.="&$key=".htmlentities($value);
+          }
       }
+      return $link;
+  }
+
+  $sql = "";
+  $total = 0;
+  /* 获取sql语句中的筛选部分 start */
+  $sql_filter = " 1 ";
+  
+  if(isset($_GET['status']) && trim($_GET['status']) != "" && trim($_GET['status']) != "all") {
+    $status = trim($_GET['status']);
+    $args['status'] = $status;
+    switch ($status) {
+      case "Available":
+        $sql_filter .= " AND `defunct`='N' ";
+      break;
+      case "Reserved":
+        $sql_filter .= " AND `defunct`='Y' ";
+      break;
     }
   }
-  $sql.=" ORDER BY `problem_id` DESC ";
-  $st=($page-1)*$page_cnt;
-  $sql.=" LIMIT $st,$page_cnt";
-
-  if($first) $sql="";
-  // get problems END
-  /* 计算页数cnt start */
-  $view_total_page=$cnt/$page_cnt+($cnt%$page_cnt?1:0);// 页数
-  /* 计算页数cnt end */
-
-  if(isset($_GET['OJ'])&& $_GET['OJ']!="" ) $href= "problem_list.php?OJ=".$_GET['OJ'];
-  if(isset($keyword)&& $keyword!=""){	  
-  	if(isset($href)&& $href!=""){
-	  $href .="&keyword=".$keyword;
-	} else {
-      $href= "problem_list.php?keyword=".$keyword;
-	}
+  
+  if(isset($_GET['keyword'])&&trim($_GET['keyword'])!="" && trim($_GET['keyword'])!="all") {
+    $keyword=htmlentities(trim($_GET['keyword']));
+    $keyword=$mysqli->real_escape_string($keyword);
+    $args['keyword']=$keyword;
+      $sql_filter .= " AND (title like '%$keyword%' or source like '%$keyword%' or author like '%$keyword%' OR tag1 like '%$keyword%' OR tag2 like '%$keyword%' OR tag3 like '%$keyword%')";
   }
-  if(isset($href)&& $href!=""){
-	$href .="&page=";
+  $problem_sets = array();
+  $result = $mysqli->query("SELECT `set_name`,`set_name_show` FROM `problemset`");    
+  while($row = $result->fetch_array()) {
+    if(HAS_PRI("edit_".$row['set_name']."_problem")) array_push($problem_sets,$row);
+  }
+  if(isset($_GET['OJ'])&&trim($_GET['OJ'])!="" && trim($_GET['OJ'])!="all") {
+    $OJ = trim($_GET['OJ']);
+    $args['OJ'] = $OJ;
+    if(HAS_PRI("edit_".$OJ."_problem")) {
+      $sql_filter .= " AND `problemset`='".$OJ."'";
+    } else $sql_filter .= " AND 0";
   } else {
-    $href= "problem_list.php?page=";
+    if($problem_sets) {
+      $sql_filter .= " AND `problemset` IN ('". implode("','", array_column($problem_sets, 'set_name')) ."')";
+    } else $sql_filter .= " AND 0";
   }
+/* 获取sql语句中的筛选部分 end */
+
+  /* 计算页数cnt start */
+  $result = $mysqli->query("SELECT COUNT('problem_id') FROM `problem` WHERE ".$sql_filter);
+  if($result) $total = $result->fetch_array()[0];
+  $page_cnt=100;
+  if(trim($_GET['keyword']) != "") { //查找结果全部显示在一页上
+    $page_cnt = $total? $total:1;
+  }
+  $st=($page-1)*$page_cnt;
+  $view_total_page=$total/$page_cnt+($total%$page_cnt?1:0);// 页数
+  /* 计算页数cnt end */
+  $sql = "SELECT p.*,s.`set_name_show` FROM `problem` AS p LEFT JOIN `problemset` AS s ON p.`problemset`=s.`set_name`";
+  $sql.= " WHERE ".$sql_filter. " ORDER BY `problem_id` DESC LIMIT $st,$page_cnt";
+  //echo "<pre>$sql</pre>";
+  $result=$mysqli->query($sql) or die($mysqli->error);
 ?>
 
   <title><?php echo $html_title.$MSG_PROBLEM.$MSG_LIST?></title>
   <h1><?php echo $MSG_PROBLEM.$MSG_LIST?></h1>
   <h4><?php echo $MSG_HELP_PROBLEM_LIST ?></h4>
   <hr/>
+  <!-- 题目查找 start -->
   <div style="margin-top: 10px;margin-bottom: 10px;">
-  <form action="problem_list.php">
-    <select class='selectpicker' onchange="location.href='<?php echo $href?>'+this.value;">
+  <form class="form-inline center" action="problem_list.php" id="searchform">
+    <select class="selectpicker show-tick" name="status" data-width="auto" onchange='javascript:document.getElementById("searchform").submit();'>
+      <option value='all' <?php if (isset($_GET['status']) && ($_GET['status'] == "" || $_GET['status'] == "all")) echo "selected"; ?>><?php echo $MSG_ALL.$MSG_STATUS ?></option>
+      <option value='Available' <?php if (isset($_GET['status']) && $_GET['status'] == "Available" ) echo "selected"; ?>><?php echo $MSG_Available ?></option>
+      <option value='Reserved' <?php if (isset($_GET['status']) && $_GET['status'] == "Reserved" ) echo "selected"; ?>><?php echo $MSG_Reserved ?></option>
+  </select>&nbsp;
+    <select class='selectpicker show-tick' data-width="auto" name="OJ" onchange='javascript:document.getElementById("searchform").submit();'>
+      <option value='all' <?php if (isset($_GET['OJ']) && ($_GET['OJ'] == "" || $_GET['OJ'] == "all")) echo "selected"; ?> ><?php echo $MSG_ALL.$MSG_PROBLEMSET ?></option>
     <?php
-      for ($i=1;$i<=$view_total_page;$i++){
-        if ($i>1) echo '&nbsp;';
-        if ($i==$page) echo "<option value='$i' selected>";
-        else  echo "<option value='$i'>";
-        echo "page ".$i;
-        echo "</option>";
+      foreach ($problem_sets as $row) {
+        echo "<option value='$row[0]'". ($row[0]==$_GET['OJ']?" selected ":" ").">$row[1]</option>";
       }
     ?>
     </select>
-    <select class='selectpicker' onchange="location.href='problem_list.php?OJ='+this.value;">
-      
-    <?php
-      echo "<option value='' selected>All</option>";
-      $res=$mysqli->query("SELECT set_name FROM problemset");
-      while($set_name=$res->fetch_array()[0]){
-        if(HAS_PRI("edit_".$set_name."_problem"))
-        echo "<option value='$set_name'". ($set_name==$_GET['OJ']?" selected ":" ").">{$set_name_show[$set_name]}</option>";
-      }
-    ?>
-    </select>
-    <input  name="keyword" type="text" placeholder="<?php echo $MSG_KEYWORDS ?>" <?php if(isset($keyword)) echo "value='$keyword'"; ?> />&nbsp;<input type=submit value="<?php echo $MSG_SEARCH?>" class="btn btn-default" >
-    </form>
+    <input class="form-control" name="keyword" type="text" placeholder="<?php echo $MSG_KEYWORDS ?>" <?php if(isset($keyword)) echo "value='$keyword'"; ?> />&nbsp;<input type=submit value="<?php echo $MSG_SEARCH?>" class="btn btn-default" >
+    </form>    
     </div>
-<?php
-  //echo "<pre>$sql</pre>";
-  $result=$mysqli->query($sql) or die($mysqli->error);
-?>
-  
+    <!-- 题目查找 end -->
+  <!-- 页标签 start -->
+  <div >
+    <ul class="pagination text-center" style="margin-top: 0px;margin-bottom: 0px;">
+        <?php $link = generate_url(Array("page"=>max($page-1, 1)))?>
+      <li><a href="<?php echo $link ?>">&laquo; Prev</a></li>
+        <?php
+        //分页
+        for ($i=1;$i<=$view_total_page;$i++){
+            $link=generate_url(Array("page"=>"$i"));
+            if($page==$i)
+                echo "<li class='active'><a href=\"$link\">{$i}</a></li>";
+            else
+                echo "<li><a href=\"$link\">{$i}</a></li>";
+        }
+        ?>
+        <?php $link = generate_url(Array("page"=>min($page+1,intval($view_total_page)))) ?>
+      <li><a href="<?php echo $link ?>">Next &raquo;</a></li>
+    </ul>
+  </div>
+<!-- 页标签 end -->
+<!-- 罗列题目 start -->
 <form id="form1" action="contest_add.php" method='post' onkeydown='if(event.keyCode==13){return false;}'>
     <table class='table table-hover table-bordered table-condensed table-striped' style='white-space: nowrap;'>
     <thead><tr>
@@ -175,7 +190,7 @@ if(isset($_GET['keyword'])&&trim($_GET['keyword'])!="") {
           $view_source .= "</div>";
           ?>
           <td style='white-space:normal;'><?php echo $view_source ?></td>
-          <td><?php echo $set_name_show[$row->problemset] ?></td>
+          <td><?php echo $row->set_name_show ?></td>
           <td><?php echo $row->in_date ?></td>
 </tr>
  <?php   }?>
@@ -185,6 +200,27 @@ if(isset($_GET['keyword'])&&trim($_GET['keyword'])!="") {
 </tr>
 </tfoot>
 </table></form>
+<!-- 罗列题目 end -->
+<!-- 页标签 start -->
+<div >
+    <ul class="pagination text-center" style="margin-top: 0px;margin-bottom: 0px;">
+        <?php $link = generate_url(Array("page"=>max($page-1, 1)))?>
+      <li><a href="<?php echo $link ?>">&laquo; Prev</a></li>
+        <?php
+        //分页
+        for ($i=1;$i<=$view_total_page;$i++){
+            $link=generate_url(Array("page"=>"$i"));
+            if($page==$i)
+                echo "<li class='active'><a href=\"$link\">{$i}</a></li>";
+            else
+                echo "<li><a href=\"$link\">{$i}</a></li>";
+        }
+        ?>
+        <?php $link = generate_url(Array("page"=>min($page+1,intval($view_total_page)))) ?>
+      <li><a href="<?php echo $link ?>">Next &raquo;</a></li>
+    </ul>
+  </div>
+<!-- 页标签 end -->
 <?php
   require_once("admin-footer.php")
 ?>
