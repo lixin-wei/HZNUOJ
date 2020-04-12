c6e2ed2f1ea73273714e630b0a1aea2a2372b78f
lixun2015 <lixun516@qq.com>
2020-04-12 20:10:32 +0800

    remove whitespace beside "="

--- web/OJ/include/static.php
+++ web/OJ/include/static.php
@@ -20,7 +20,7 @@
   $OJ_HOME=dirname(dirname(__FILE__));//:OJ/ OJ的首页地址
   static  $OJ_NAME="HZNUOJ";//OJ的名字，将取代页面标题等位置HZNUOJ字样。
   static  $OJ_ADMIN="root@localhost";//管理员email
-  static  $OJ_DATA = "/home/judge/data";//测试数据所在目录，实际位置。
+  static  $OJ_DATA="/home/judge/data";//测试数据所在目录，实际位置。
   static  $OJ_ONLINE=false;//是否使用在线监控，需要消耗一定的内存和计算，因此如果并发大建议关闭
   static  $OJ_LANG="en";//设置默认显示的语言，中文为cn，英文为en
   static  $OJ_SIM=true;//是否显示相似度检测的结果。
@@ -30,14 +30,14 @@
                 //1 1111111111111111111=1(524287D)=1048575 语言全开
                 //1 1101111001111111111=1(455679D)=979967 github上down下来默认JavaScript、Obj-C、FreeBasic不开
                 //1 0101111001111111111=1(193535D)=717823 为方便和hustoj对接，关闭18Python3；Python2、Python3可以通过系统配置切换，具体参看wiki/maintainer-manual.md
-							  //1 0000000000001000011 = 1(67D)=524355 只开C C++ python
+							  //1 0000000000001000011=1(67D)=524355 只开C C++ python
 							  //试验了下最高位1不补好像也没问题
   static  $OJ_EDITE_AREA=true; //true: 是否启用高亮语法显示的提交界面，可以在线编程，无须IDE。
   static  $OJ_AUTO_SHARE=true; //true: 自动分享代码，启用的话，做出一道题就可以在该题的Status中看其他人的答案。
   static  $OJ_CSS="hoj.css";
   static  $OJ_SAE=false; //是否是在新浪的云平台运行web部分
-  static  $OJ_VCODE = true; // 是否开启验证码
-  static  $OJ_APPENDCODE = true; // 是否启用自动添加代码，启用的话，提交时会参考$OJ_DATA对应题目的目录里是否有append.c、prepend.c一类的文件，
+  static  $OJ_VCODE=true; // 是否开启验证码
+  static  $OJ_APPENDCODE=true; // 是否启用自动添加代码，启用的话，提交时会参考$OJ_DATA对应题目的目录里是否有append.c、prepend.c一类的文件，
                                  //有的话会把其中代码附加到对应语言的提交代码之前（prepend.c加到C语言代码之前）或之后（append.c加到C语言代码之后），
                                  //C++代码是append.cc、append.cc等，对应的后缀名见/include/const.inc.php的$language_ext数组，
                                  //巧妙使用可以指定main函数而要求学生编写main部分调用的函数。
@@ -48,16 +48,16 @@
   static  $OJ_LOGIN_MOD="hustoj";
   static  $OJ_RANK_LOCK_PERCENT=0.2; //比赛封榜时间的比率，如5小时比赛设为0.2则最后1小时封榜。
   static  $OJ_SHOW_DIFF=true;//是否显示WA的对比说明
-  static  $OJ_TEST_RUN = false;//提交界面是否允许测试运行
-  static $OJ_OPENID_PWD = '8a367fe87b1e406ea8e94d7d508dcf01';
+  static  $OJ_TEST_RUN=false;//提交界面是否允许测试运行
+  static $OJ_OPENID_PWD='8a367fe87b1e406ea8e94d7d508dcf01';
 
 
-  static $ICON_PATH = "image/hznuoj.ico";
-  static $GOLD_RATE = 0.10; // 金牌比例
-  static $SILVER_RATE = 0.20; // 银牌比例
-  static $BRONZE_RATE = 0.30; // 铜牌比例
-  static $BORDER = 500000;
-  static $LOGIN_DEFUNCT = false;
+  static $ICON_PATH="image/hznuoj.ico";
+  static $GOLD_RATE=0.10; // 金牌比例
+  static $SILVER_RATE=0.20; // 银牌比例
+  static $BRONZE_RATE=0.30; // 铜牌比例
+  static $BORDER=500000;
+  static $LOGIN_DEFUNCT=false;
   static $VIDEO_SUBMIT_TIME=3;// can see video after
 
   static  $OJ_REGISTER=true; //允许注册新用户
@@ -69,8 +69,8 @@
   static  $OJ_NEED_CLASSMODE=true;//是否开启班级模式，包括显示班级、学号、真名
 
   /* Email configuration */
-  static $SMTP_SERVER = "smtp.exmail.qq.com";
-  static $SMTP_SERVER_PORT = 25;
-  static $SMTP_USER = "forgot@hsacm.com";
-  static $SMTP_PASS = "hznuojForgot123";
+  static $SMTP_SERVER="smtp.exmail.qq.com";
+  static $SMTP_SERVER_PORT=25;
+  static $SMTP_USER="forgot@hsacm.com";
+  static $SMTP_PASS="hznuojForgot123";
 ?>
