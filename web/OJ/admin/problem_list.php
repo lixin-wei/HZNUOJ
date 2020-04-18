eb9206d2eabecec88ae539b1c7482e97b1f118e6
lixun2015 <lixun516@qq.com>
2020-04-18 22:07:39 +0800

    修改：来源/分类标签的添加使用统一的js.php

--- web/OJ/admin/problem_list.php
+++ web/OJ/admin/problem_list.php
@@ -300,27 +300,6 @@
   </div>
 <!-- 页标签 end -->
 <?php
-  require_once("admin-footer.php")
+  require_once("admin-footer.php");
+  include("../template/$OJ_TEMPLATE/js.php");
 ?>
-<script type="text/javascript">
-var color_theme=["primary","secondary","success","warning","danger"];
-function problem_add_source(sp,pid){
-  //console.log("pid:"+pid);
-  let p=$(sp).parent();
-  p.html("<form onsubmit='return false;'><input type='hidden' name='m' value='problem_add_source'><input type='hidden' name='ppid' value='"+pid+"'><input type='text' name='ns' maxlength='20'><input type='button' value='<?php echo $MSG_ADD ?>'></form>");
-  p.find("input[name=ns]").focus();
-  p.find("input[name=ns]").change(function(){
-    //console.log($("#form1").serialize());
-    let ns=p.find("input[name=ns]").val();
-    //console.log("new source:"+ns);
-    $.post("./ajax.php",$("#form1").serialize(),function(data,textStatus) {
-      if(textStatus=="success") {
-        if(data!=0) {
-          p.parent().append("<a title='"+ns+"' class='am-badge am-badge-"+color_theme[Math.floor(Math.random()*5)]+" am-text-sm am-radius' href='problemset.php?search=" +ns+ "'>" +(ns.length>10 ? ns.substr(0,10)+"…" : ns) + "</a>&nbsp;");
-        } else alert("‘"+ns+"’已存在！");
-        p.html("<span class='am-icon-plus' pid='"+pid+"' style='cursor: pointer;' onclick='problem_add_source(this,"+pid+");'></span>");
-      }
-    });
-  });
-}
-</script>
--- web/OJ/template/hznu/problemset.php
+++ web/OJ/template/hznu/problemset.php
@@ -205,7 +205,10 @@ function generate_url($data){
 </div>
 <!-- 页标签 end -->
 
-<?php require_once("footer.php") ?>
+<?php
+  require_once("footer.php");
+  include("js.php");
+?>
 
 <!-- problem selector js START-->
 <script type="text/javascript">
@@ -230,26 +233,6 @@ function generate_url($data){
   //echo "<pre>$id</pre>";
 ?>
 <script type="text/javascript">
-var color_theme=["primary","secondary","success","warning","danger"];
-function problem_add_source(sp,pid){
-  //console.log("pid:"+pid);
-  let p=$(sp).parent();
-  p.html("<form onsubmit='return false;'><input type='hidden' name='m' value='problem_add_source'><input type='hidden' name='ppid' value='"+pid+"'><input type='text' class='input input-large' name='ns' maxlength='20'><input type='button' value='<?php echo $MSG_ADD ?>'></form>");
-  p.find("input[name=ns]").focus();
-  p.find("input[name=ns]").change(function(){
-    //console.log(p.find("form").serialize());
-    let ns=p.find("input[name=ns]").val();
-    //console.log("new source:"+ns);
-    $.post("admin/ajax.php",p.find("form").serialize(),function(data,textStatus) {
-      if(textStatus=="success") {
-        if(data!=0) {
-          p.parent().append("<a title='"+ns+"' class='am-badge am-badge-"+color_theme[Math.floor(Math.random()*5)]+" am-text-default am-radius' href='problemset.php?search=" +ns+ "'>" +(ns.length>10 ? ns.substr(0,10)+"…" : ns) + "</a>&nbsp;");
-        } else alert("‘"+ns+"’已存在！");
-        p.html("<span class='am-icon-plus' pid='"+pid+"' style='cursor: pointer;' onclick='problem_add_source(this,"+pid+");'></span>");
-      }
-    });
-  });
-}
   $("#random_choose").click(function(){
     window.location.href="problem.php?id=<?php echo $id; ?>";
   });
