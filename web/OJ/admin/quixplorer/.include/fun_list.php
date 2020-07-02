b07f8bfe4a5a269a289243b112e91f4b792d7433
lixun2015 <lixun516@qq.com>
2020-07-02 09:37:39 +0800

    fixed for php7.2

--- web/OJ/admin/quixplorer/.include/fun_list.php
+++ web/OJ/admin/quixplorer/.include/fun_list.php
@@ -54,13 +54,13 @@ function make_list($_list1, $_list2) {		// make list of files
 	}
 	
 	if(is_array($list1)) {
-		while (list($key, $val) = each($list1)) {
+		foreach ($list1 as $key => $val) {
 			$list[$key] = $val;
 		}
 	}
 	
 	if(is_array($list2)) {
-		while (list($key, $val) = each($list2)) {
+		foreach ($list2 as $key => $val){
 			$list[$key] = $val;
 		}
 	}
@@ -144,8 +144,7 @@ function make_tables($dir, &$dir_list, &$file_list, &$tot_file_size, &$num_items
 function print_table($dir, $list)
 {
 	if(!is_array($list)) return;
-	
-	while(list($item,) = each($list)){
+	foreach ($list as $item => $val) { //while(list($item,) = each($list)){ // php7.2 error:each()
 		// link to dir / file
 		$abs_item=get_abs_item($dir,$item);
 		$target="";
